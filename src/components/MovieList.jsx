0db96b2ef6f3457c0243e143bc730b1fac027bbd
wellington <profwellington17@gmail.com>
2020-11-09 20:41:41 -0300

    fix4 requisito 3-5  #VQV!

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-  render() {
-    const listMovies = this.props.movies;
-    return (
-      <div>
-        {listMovies.map((movie) => <MovieCard movie={movie} key={movie.title} />)}
-      </div>
-    );
-  }
+render() {
+const listMovies = this.props.movies;
+return (
+<div>
+{listMovies.map((movie) => <MovieCard movie={movie} key={movie.title} />)}
+</div>
+);
+}
 }
 export default MovieList;

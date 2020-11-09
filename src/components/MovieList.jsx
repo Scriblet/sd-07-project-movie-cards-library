7dcac328cd469041b2820bb0e378823dcd34aaa9
Carol Andrade <a.carolg1@gmail.com>
2020-11-09 16:58:41 -0300

    update

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
 // implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div className="movie-list">
+        {this.props.movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+      </div>
+    );
+  }
+}
+
+// source = https://pt-br.reactjs.org/docs/typechecking-with-proptypes.html
+MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object) };
+
+
+export default MovieList;

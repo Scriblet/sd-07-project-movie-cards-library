5ddee0580fb1f3d0956640446777845257009c2a
renatomak <renato.mark.silva@gmail.com>
2020-11-09 15:19:54 -0300

    requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+const { Component } = require("react")
+
+import React, { Component } from 'react';
+import movies from '../data';
+
+class MovieList extends Component {
+    render() {
+      return (
+        <main>
+
+        </main>
+      );
+    }
+}
+
+export default MovieList;

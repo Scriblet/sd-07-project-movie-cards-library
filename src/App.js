830b39f37ed2ca0da5d79852813538da68afdaeb
Pablo <pablopessanh@gmail>
2020-11-09 14:44:05 -0300

    requirements 3rd, 4th, 5th and 6th done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import movies from './data';
 import './App.css';
 
 function App() {
@@ -7,7 +9,7 @@ function App() {
     <div className='App'>
       {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
       <Header />
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      <MovieList movies={movies} />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,17 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    const { movie } = this.props;
+    const { title } = movie;
+
+    return (
+      <div className='movie-card' key={title}>
+        <span>Olá</span>
+      </div>
+    );
+  };
+};
+
+export default MovieCard;

8566a718d4a0e8d53610853f3473ee1860e5087b
Berilo Wanderley <wberilo@gmail.com>
2020-11-09 16:45:52 -0300

    init, all requirements

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,17 @@
 // implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const {movie} = this.props
+    return (<div>
+      <h4>{movie.title}</h4>
+      <h5>{movie.subtitle}</h5>
+      <p>{movie.storyline}</p>
+      <Rating rating={movie.rating}/>
+      <img src={movie.imagePath}></img>
+    </div>)
+  }
+}
+export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,11 @@
 // implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    const rating = this.props.rating;
+    return <h3 className='rating'>{rating}</h3>
+  }
+}
+
+export default Rating;

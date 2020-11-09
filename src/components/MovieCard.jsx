2c8def4624eeee5e43e6952f1e34ee55cd5a4563
Bruno <bruno_grb@hotmail.com>
2020-11-09 15:21:37 -0300

    adicionando arquivos dos componentes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,21 @@
-// implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render () {
+    const {title, subtitle, storyline, imagePath, rating} = this.props.movie;
+    return (
+      <div className="movie-card">
+        <img className="movie-card-image" src={imagePath} alt={title}/>
+        <div className="movie-card-body">
+          <div className="movie-card-title">{title}</div>
+          <div className="movie-card-subtitle">{subtitle}</div>
+          <div className="movie-card-storyline ">{storyline}</div>
+        </div>
+        <Rating rating={rating}/>
+      </div>
+      );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render () {
+    const movies = this.props.movies;
+    return (
+      <MovieCard movie={movies}/>
+      );
+  }
+}
+
+export default MovieList;

3190d8a967beb9eb7ac2812bc7f7fd1cc154b73a
Vanessa Oliveira <vanessanaara@gmail.com>
2020-11-09 19:22:43 -0300

    Retirando linhas 5 e 7 do arquivo App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import './App.css';
 import data from './data';
 import Header from './components/Header';
-// import MovieCard from './components/MovieCard';
 import MovieList from './components/MovieList';
-// import Rating from './components/Rating';
 
 function App() {
   return (

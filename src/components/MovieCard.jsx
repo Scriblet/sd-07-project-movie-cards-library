1d6b8bf089297541925edfc2472215b60ead5657
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-11-09 17:15:18 -0300

    requisito 6, 7 e 8

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,20 @@
 // implement MovieCard component here
 import React from 'react';
+import PropsTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    return <div />;
+    const movie = this.props.movie;
+
+    return (
+      <div>
+        <img src={movie.imagePath} alt={movie.title} />
+        <h4>{movie.title}</h4>
+      </div>
+    );
   }
 }
 
 export default MovieCard;
+
+MovieCard.propTypes = { movie: PropsTypes.objectOf().isRequired };
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -8,7 +8,7 @@ class MovieList extends React.Component {
     const movies = this.props.movies;
 
     return (
-        movies.map((movie) => <MovieCard movies={movie} />)
+        movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)
     );
   }
 }

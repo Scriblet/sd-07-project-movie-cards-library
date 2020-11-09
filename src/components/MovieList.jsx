1cb317527bc143159ab9beda627bb16c47ceeaa0
Fernando Antunes <fernando.antunes1@gmail.com>
2020-11-09 15:58:04 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
 // implement MovieList component here
+import React from 'react';
+import movies from '../data'
+
+class MovieList extends React.Component {
+  render() {
+		return(
+			<p>{ movies[0].title }</p>
+		);
+	}
+}
+
+export default MovieList;

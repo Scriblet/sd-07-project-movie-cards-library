9d09286494d50fd8bad88c290b8aa0fe193f3592
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-11-09 18:39:23 -0300

    Requisito 17 - Adicione proptypes a todos os componentes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
-// import propTypes from 'prop-types';
+import propTypes from 'prop-types';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const {title, subtitle, storyline, imagePath, rating} = this.props.movie;
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <div>
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
-        <Rating rating={rating}/>
+        <Rating rating={rating} />
         <p>{storyline}</p>
-        <img src={imagePath}></img>
+        <img src={imagePath} alt={title}/>
       </div>
     );
   }
 }
 
 export default MovieCard;
+
+MovieCard.propTypes = {
+  movie: propTypes.shape({
+      title: propTypes.string,
+      subtitle: propTypes.string,
+      storyline: propTypes.string,
+      rating: propTypes.number,
+      imagePath: propTypes.string,
+    }),
+};
+
+MovieCard.defaultProps = { movies: {} };

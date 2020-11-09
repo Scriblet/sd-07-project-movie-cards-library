4a6034de1424235e8f82e0c730b50541fe7b55e7
brendalima <brenda.c.lima@poli.ufrj.br>
2020-11-09 18:36:01 -0300

    attempt at requirement 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,10 @@
 // implement MovieList component here
 import React from 'react';
+import movies from './src/data.js';
 
 class MovieList extends React.Component {
   render() {
-    return <div></div>;
+  return <div>{ this.props.movies }</div>;
   }
 }
 

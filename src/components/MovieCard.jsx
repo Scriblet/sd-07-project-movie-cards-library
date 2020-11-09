b843de22271f08d261c7c15087eb67a87e72f4d3
Lets <letsferreiratx@gmail.com>
2020-11-09 18:51:44 -0300

    8ao15 ok

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -7,6 +8,10 @@ class MovieCard extends React.Component {
     return (
       <div>
         <img src={imagePath} alt="" />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating rating={rating} />
       </div>
     );
   }

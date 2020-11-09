0d3c619e6f3c866cc87edf84cd2c50e5b31df4f8
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-11-09 17:00:14 -0300

    movie list, movie card criado sem rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,28 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath } = this.props.movie;
+    return (
+      <div>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <img alt='' src={imagePath}></img>
+      </div>
+)
+  }
+}
+
+MovieCard.propTypes = {
+  movies: PropTypes.shape({
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline:PropTypes.string,
+  image: PropTypes.string,
+  rating: PropTypes.number,
+})
+};
+
+export default MovieCard;

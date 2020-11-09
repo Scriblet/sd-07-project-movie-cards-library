7aef5304439e98aea88423b8c15a6f06544f0a31
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-11-09 16:11:47 -0300

    Codeclimate

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Rating extends Component {
   render() {
-    return <p className='rating'>{this.props.rating}</p>;
+    return <p className="rating">{this.props.rating}</p>;
   }
 }
 

f39b422a73a4e80e87d66c8976c41ed253e1de4d
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-11-09 16:20:33 -0300

    requisitos 1 e 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 class Header extends React.Component {
-    render() {
-
-    }
-};
+  render() {
+    return (<h1 className="movie-card-header">Movie Cards Library</h1>);
+  }
+}
 
 export default Header;

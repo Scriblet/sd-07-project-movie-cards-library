4f9e1a2081a279d1b503b1af2f2260e1cc84f55d
Anderson Santana <anderslash@gmail.com>
2020-11-09 16:16:30 -0300

    Componente MovieList criado - Requisito 04

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render () {
+    return (
+      <div>
+          Teste
+          {console.log(this.props)}
+      </div>
+    )
+  }
+}
+
+export default MovieList;

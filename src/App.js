321e523ea478c0387da1d3739387141d75ca21b5
Lucas Miranda <lucasmiranda@pop-os.localdomain>
2020-11-09 16:30:28 -0300

    code climate single quote teste

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Header from './components/Header';
-import './App.css';
 
 function App() {
   return (
-<div className="App">
-  <Header />
-</div>
+    <div className="App">
+      <Header />
+    </div>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <header className="movie-card-header">
-        <h1 className="page-title">Movie Cards Library</h1>
-      </header>
+      <div>
+        <header className="movie-card-header">
+          <h1 className="page-title">Movie Cards Library</h1>
+        </header>
+      </div>
     );
   }
 }

0f7f5fcda971ae2ad2a4014c1b65411718ba50f2
moraisgabri <gabriel8azevedo@gmail.com>
2020-11-09 17:02:15 -0300

    tentativa comit

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,21 +2,18 @@ import React, { Component } from 'react';
 import Rating from './Rating';
 
 class MovieCard extends Component {
-  render () {
-    const { movie } = this.props;
-    const { title, subtitle, storyline, rating, imagePath } = movie
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie
     return (
-    <div className={title}>
-      <img src={imagePath} alt={title}/>
-      <h4>{title}</h4>
-      <h5>{subtitle}</h5>
-      <p>{storyline}</p>
-      <Rating rating={rating}/>
-    </div> 
-    );
+      <div className='movie-card'>
+        <img src={imagePath} alt={title}/>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating rating={rating} />
+      </div>
+    )
   }
 }
-
-
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import MovieCard from './MovieCard';
 
+
 class MovieList extends Component {
   render() {
-    return (
-      <div className="movie-list">
-        { this.props.movies.map((movie) => <MovieCard key={movie.title} movie={movie} />) }
+    return(
+      <div className='movie-list'>
+        { this.props.movies.map(movie => <MovieCard movie={movie} key={movie.title} />) }
       </div>
-      );
+    )
   }
 }
 

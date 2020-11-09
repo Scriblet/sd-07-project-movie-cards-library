a1ca4c69b80102ca0af10d65fca5927be256ee83
renatomak <renato.mark.silva@gmail.com>
2020-11-09 14:54:50 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ class Header extends Component {
     render(){
 
         return (
-            <div> <h1>Movie Cards Library</h1></div>
+            <header> <h1>Movie Cards Library</h1></header>
         )
     };
 }

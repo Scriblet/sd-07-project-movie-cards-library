3d9ec5d076125ab34699c636f99dc6518a87c96b
murilopereira006 <muriloedu006@gmail.com>
2020-11-09 18:08:56 -0300

    entre o ex 6 ou7 mas tudo interligado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,16 @@
-// implement MovieCard component here
+import React from 'react';
+import movies from './../data';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+    render() {
+        const { title, subtitle, storyline, imagePath, rating } = movies;
+
+        return(
+
+        )
+    }
+}
+
+export default MovieCard;
+

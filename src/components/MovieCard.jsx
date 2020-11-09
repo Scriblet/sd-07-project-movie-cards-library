28541bee0d2e11c0bd97f66e0e6b0604c6ec1a05
Lets <letsferreiratx@gmail.com>
2020-11-09 20:39:10 -0300

    css ok ok

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
+
 
 class MovieCard extends React.Component {
   render() {
@@ -7,11 +9,13 @@ class MovieCard extends React.Component {
 
     return (
       <div className="movie-card">
+        <img className="movie-card-image" src={imagePath} alt="" />
         <div className="movie-card-body">
-          <img className="movie-card-image" src={imagePath} alt="" />
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
+        </div>
+        <div className="movie-card-rating">
           <Rating rating={rating} />
         </div>
 
@@ -19,5 +23,14 @@ class MovieCard extends React.Component {
     );
   }
 }
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 class Rating extends React.Component {
   render() {
-    const { rating } = this.props.rating;
-
     return (
       <div className="rating">
-        <span className="movie-card-rating">{rating}</span>
+        {this.props.rating}
       </div>
     );
   }

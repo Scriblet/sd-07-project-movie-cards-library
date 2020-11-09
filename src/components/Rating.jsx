588529132f6791647d9a9ccdd643af4b76e2d992
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-11-09 19:07:33 -0300

    mais uma tentativa

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
-class Rating extends React.Component {
+class Rating extends Component {
   render() {
-    const { rating } = this.props;
-    return <p className="rating">{rating}</p>;
+    return <p className="rating">{this.props.rating}</p>;
   }
 }
 
-Rating.protoTypes = { rating: PropTypes.number.isRequired };
+Rating.propTypes = { rating: PropTypes.number.isRequired };
 export default Rating;

dc91c2b749e0b890816d621fe13230e9bb81daf0
Israel Menon <zezomenon@gmail.com>
2020-11-09 18:23:05 -0300

    15 App deve renderizar Header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import movies from './data';
 import MovieList from './components/MovieList';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <MovieList movies={movies} />
     </div>
   );

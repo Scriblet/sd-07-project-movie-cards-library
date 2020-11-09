4d9d4553196953eeb4fef866e49c1d219e53ed47
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-11-09 15:06:34 -0300

    redering moviecard

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const movies = this.props.movies;
+
+    return (
+      <div>
+        {movies.map((movie) => <MovieCard movies={movie} /> )}
+      </div>
+    );
+  }
+}
+
+export default MovieList;

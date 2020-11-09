6b8acc6d57bd43af90716a9ce50cfb191932476f
Jr3564 <rodolfojr94@gmail.com>
2020-11-09 18:30:48 -0300

    codeclimate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,9 @@
-import React from "react";
+import React from 'react';
 
-class Header extends React.Component {
-  render () {
-    return <header className='movie-card-header'>
-        <h1 className='page-title'>{this.props.children}</h1>
-      </header>;
+export default class Header extends React.Component {
+render () {
+    return (<header className="movie-card-header">
+      <h1 className="page-title">{this.props.children}</h1>
+    </header>);
   }
-};
-
-export default Header;
+}
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import Rating from './Rating.jsx';
 
 export default class MovieCard extends React.Component {

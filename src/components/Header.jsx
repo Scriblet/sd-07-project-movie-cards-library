b87f5eadb2562c355d2e326b2b234faec7232428
CleberFontinele <cleberfontinele2387@gmail.com>
2020-11-09 15:50:34 -0300

    Crie um componente chamado Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// Iniciando o projeto 🚀
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello Wolrd!
+            </div>
+        )
+    }
+}
+
+export default Header;

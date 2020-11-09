974270a3b33efcf486fe4036110450c969f93d56
Strongreen <strongreen@live.com>
2020-11-09 19:46:24 -0300

    correçao CC

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,21 +4,17 @@ import Movies from '../data.js';
 
 class MovieCard extends React.Component {
   render() {
-    return Movies.map((movies) => 
-        <div className="movie-card">
-          <h4 className="movie-card-title">{movies.title}</h4>
-          <h5 className="movie-card-subtitle">{movies.subtitle}</h5>
-          <img
-            className="movie-card-image"
-            src={movies.imagePath}
-            alt="Imagem"
-          />
-          <p className="movie-card-storyline">{movies.storyline}</p>
-          <p className="movie-card-rating">
-            <Rating />
-          </p>
-        </div>
-     );
+    return Movies.map((movies) => (
+      <div className="movie-card">
+        <h4 className="movie-card-title">{movies.title}</h4>
+        <h5 className="movie-card-subtitle">{movies.subtitle}</h5>
+        <img className="movie-card-image" src={movies.imagePath} alt="Imagem" />
+        <p className="movie-card-storyline">{movies.storyline}</p>
+        <p className="movie-card-rating">
+          <Rating />
+        </p>
+      </div>
+    ));
   }
 }
 

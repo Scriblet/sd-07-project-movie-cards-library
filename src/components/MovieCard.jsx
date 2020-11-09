6e532dc092b9d2c2ea1a7945e8ddf2c92c740366
alvaro <alvarovascon2@gmail.com>
2020-11-09 16:38:05 -0300

    setting proptypes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Rating from './Rating';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
       <div className="movie-card">
-        <img className="movie-card-image" src={movie.imagePath} alt=""/>
+        <img className="movie-card-image" src={movie.imagePath} alt="" />
         <h4 className="movie-card-title">{movie.title}</h4>
         <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
         <p className="movie-card-storyline">{movie.storyline}</p>
@@ -14,6 +15,16 @@ class MovieCard extends React.Component {
       </div>
     );
   }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })
 }
 
 export default MovieCard;

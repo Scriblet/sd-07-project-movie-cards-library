bd1bad0fefaf64bfc9bc06a5741f08009a3f4119
Emerson Moonlit <emoonlit@pop-os.localdomain>
2020-11-09 17:22:21 -0300

    Movie Card and Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
     render() {
+        const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
         return (
-          <div>
-              
-          </div>
+            <div className='movie-card'>
+                <img src={imagePath} alt={{title} + " " + {subtitle}}/>
+                <div>
+                    <h4>{title}</h4>
+                    <h5>{subtitle}</h5>
+                    <p>{storyline}</p>
+                </div>
+                <div>
+                    <Rating rating={rating}/>
+                </div>
+            </div>
         )
     }
 }

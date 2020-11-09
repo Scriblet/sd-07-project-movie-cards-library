e2d34266f177c9eac4cfabe4d7b19cb9cd9b0d06
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-11-09 17:52:27 -0300

    Finalizando

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,6 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import MovieCard from './MovieCard';
-import PropTypes from 'prop-types';
 
 class MovieList extends Component {
   render() {
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
 class Rating extends Component {
   render() {
     return <p className="rating">{this.props.rating}</p>;

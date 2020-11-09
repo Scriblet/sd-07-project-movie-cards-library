2c9a77109820decbc79440a0490337fe8731dedf
gabrieldeori <deori.pessoal@gmail.com>
2020-11-09 18:16:15 -0300

    MovieList.jsx | Gold

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,30 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-    render() {
-        const movies = this.props.movies;
-        return (
-        <div>
-            { movies.map(movie => <MovieCard key={`${movie.title}`} movie={movie} />)}
-        </div>
-        );
-    }
+  render() {
+    const movies = this.props.movies;
+    console.log(movies);
+    return (
+      <div>
+        { movies.map((movie) => <MovieCard key={`${movie.title}`} movie={movie} />)}
+      </div>
+    );
+  }
 }
 
 export default MovieList;
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    storyline: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  })),
+};
+
+MovieList.defaultProps = { movies: [] };

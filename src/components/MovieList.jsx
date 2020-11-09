05201a338a1110570ae61d20e6a30b9739316fe0
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-11-09 15:16:38 -0300

    Finalizado projeto sem estilizar

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
-// implement MovieList component here
+import React, { Component } from 'react'
+import MovieCard from './MovieCard'
+class MovieList extends Component {
+    render(){
+        return (
+            this.props.movies.map(movie => <MovieCard movie={movie} key={movie.title} />)
+        )
+    }
+}
+
+export default MovieList

c22b4f805d01e2b0b45c9a127c97b5ceafcd5bc7
danielmlduarte <danielduarte.tryber@gmail.com>
2020-11-09 14:22:17 -0300

    Commit inicial - Subindo projeto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const movieList = this.props.movies;
+    return (
+      <div className="movie-list">
+        { movieList.map(movie => <MovieCard key={ movie.title } movie={ movie } />) }
+      </div>
+    );
+  }
+}
+
+export default MovieList;

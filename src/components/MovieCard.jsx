ab804c92f8573899ce8823f55ec1a69baec3ac04
Strongreen <strongreen@live.com>
2020-11-09 19:39:05 -0300

    correçao CC

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,8 +4,7 @@ import Movies from '../data.js';
 
 class MovieCard extends React.Component {
   render() {
-    return Movies.map((movies) => {
-      return (
+    return Movies.map((movies) => 
         <div className="movie-card">
           <h4 className="movie-card-title">{movies.title}</h4>
           <h5 className="movie-card-subtitle">{movies.subtitle}</h5>
@@ -19,8 +18,7 @@ class MovieCard extends React.Component {
             <Rating />
           </p>
         </div>
-      );
-    });
+     );
   }
 }
 

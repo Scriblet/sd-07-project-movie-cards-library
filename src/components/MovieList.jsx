e6c8822e4a509f2f92d153bf204dca6cf8bb3a9c
Daniel Carvalho <daanwhat@gmail.com>
2020-11-09 14:43:56 -0300

    odeio CC

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,6 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import MovieCard from './MovieCard';
-import PropTypes from 'prop-types';
 
 class MovieList extends Component {
   render() {
@@ -10,8 +10,6 @@ class MovieList extends Component {
   }
 }
 
-MovieList.propTypes = {
-  movie: PropTypes.object.isRequired,
-};
+MovieList.propTypes = { movies: PropTypes.array.isRequired, };
 
 export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,13 +5,10 @@ class Rating extends Component {
   render() {
     const { rating } = this.props;
 
-    return <h6 className='rating'>{rating}</h6>;
+    return <h6 className="rating">{rating}</h6>;
   }
 }
 
-Rating.propTypes = {
-    rating: PropTypes.number.isRequired
-}
-
+Rating.propTypes = { rating: PropTypes.number.isRequired, };
 
 export default Rating;

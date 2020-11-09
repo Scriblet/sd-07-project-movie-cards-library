613aaf7a01c145b88c087a20fbdd581fbfecb0b4
Emerson Moonlit <emoonlit@pop-os.localdomain>
2020-11-09 16:04:16 -0300

    Header Component CC

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header.jsx';
 
 class App extends React.Component {
   render() {
-    return(
+    return (
       <div className="App">
         <Header />
       </div>

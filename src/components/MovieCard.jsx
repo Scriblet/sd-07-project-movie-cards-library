e0e662b11f94ce65651e9f63e6b7851ba1eb17db
moraisgabri <gabriel8azevedo@gmail.com>
2020-11-09 17:58:32 -0300

    agora vai crendeuspai todo poderoso

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,8 +6,8 @@ class MovieCard extends Component {
   render() {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
-      <div className='movie-card'>
-        <img src={imagePath} alt={title}/>
+      <div className="movie-card">
+        <img src={imagePath} alt={title} />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
@@ -18,11 +18,13 @@ class MovieCard extends Component {
 }
 
 MovieCard.propType = {
-  title: PropType.string,
-  subtitle: PropType.string,
-  storyline: PropType.string,
-  rating: PropType.number,
-  imagePath: PropType.string,
-}
+  movie: PropType.shape({
+    title: PropType.string,
+    subtitle: PropType.string,
+    storyline: PropType.string,
+    rating: PropType.number,
+    imagePath: PropType.string,
+  }),
+};
 
 export default MovieCard;

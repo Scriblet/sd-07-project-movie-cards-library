c53ca3b9135025279f4729ab4aa8acd255a91490
moacyrrln <moacyrrln@gmail.com>
2020-11-09 18:29:34 -0300

    requisito1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,12 @@
 // implement Header component here 
+import React from 'react'
+
+class HeaderClass extends React.Component {
+  render() {
+    return (
+      <header>Header</header>
+    )
+  }
+}
+
+export default HeaderClass

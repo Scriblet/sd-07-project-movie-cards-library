0ee2d74985904188112d1c6ab81211d10cd924ee
alvaro <alvarovascon2@gmail.com>
2020-11-09 16:19:00 -0300

    adding classes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,13 +4,13 @@ import Rating from './Rating';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    return(
-      <div>
-        <img src={movie.imagePath}/>
-        <h4>{movie.title}</h4>
-        <h5>{movie.subtitle}</h5>
-        <p>{movie.storyline}</p>
-        <Rating rating={movie.rating} />
+    return (
+      <div className="movie-card">
+        <img className="movie-card-image" src={movie.imagePath} alt=""/>
+        <h4 className="movie-card-title">{movie.title}</h4>
+        <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
+        <p className="movie-card-storyline">{movie.storyline}</p>
+        <Rating className="movie-card-rating" rating={movie.rating} />
       </div>
     );
   }

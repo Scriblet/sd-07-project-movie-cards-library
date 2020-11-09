dee1ad2a117eabdf895caf58d1ddf3733331d242
Strongreen <strongreen@live.com>
2020-11-09 20:32:42 -0300

    correçao do Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,14 +4,14 @@ import Movies from '../data.js';
 
 class MovieCard extends React.Component {
   render() {
-    return Movies.map((movies) => (
+    return Movies.map((movies, index) => (
       <div className="movie-card">
+        <img className="movie-card-image" src={movies.imagePath} alt="Imagem" />
         <h4 className="movie-card-title">{movies.title}</h4>
         <h5 className="movie-card-subtitle">{movies.subtitle}</h5>
-        <img className="movie-card-image" src={movies.imagePath} alt="Imagem" />
         <p className="movie-card-storyline">{movies.storyline}</p>
         <p className="movie-card-rating">
-          <Rating />
+          <Rating rating={movies.title} key={index}/>
         </p>
       </div>
     ));
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import RatinigNote from '../data.js';
 
 function getNota(title) {
-  return RatinigNote.map((note) => (note.title === title ? note.rating : 'não encontrado'));
+  return RatinigNote.map((note) => (note.title === title ? note.rating : ''));
 }
 
 class Rating extends React.Component {
   render() {
+    const { rating,index } = this.props
     return (
-      <div className="rating">
-        <p> {getNota('Kingsglaive')}</p>
+      <div key={index} className="rating">
+        { getNota(rating) }
       </div>
     );
   }

dffaa89086f385df0f759e902690126e661f75e3
Lfocosi <luccafocosi@gmail.com>
2020-11-09 18:12:47 -0300

    cc 6

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,15 +3,14 @@ import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    const nota = this.props.rating;
     return (
       <div className="movie-card-rating">
-        <p className="rating">{nota}</p>
+        <p className="rating">{this.props.rating}</p>
       </div>
     );
   }
 }
 
-Rating.prototype = { rating: PropTypes.number, };
+Rating.prototype = { rating: PropTypes.number };
 
 export default Rating;

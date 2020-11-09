99b96d7a4de58216f39111ff6261d0c80ba63c7c
vanderson-henrique <vhbenedito@gmail.com>
2020-11-09 15:24:46 -0300

    Requisitos 1 a 16

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+import MovieCard from "./MovieCard";
 
 class MovieList extends React.Component {
-    render() {
-
-    }
-}
+  render() {
+    const movies = this.props.movies;
+    return movies.map((movie) => <MovieCard key={movie.title} movie={movie} />);
+  };
+};
 
 export default MovieList;

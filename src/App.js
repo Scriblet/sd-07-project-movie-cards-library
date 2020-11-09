3a624461db0343a1fc4bf16003b95c4e8258d314
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-11-09 15:25:40 -0300

    Codeclimate

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header'
 import MovieList from './components/MovieList';
-import data from './data';
+import data from './data'
 function App() {
-	return (
-		<div className="App">
-			{/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-			<Header />
-			<MovieList movies={data} />
-		</div>
-	);
+  return (
+    <div className="App">
+      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
+      <Header />
+      <MovieList movies={data} />
+    </div>
+  );
 }
 
 export default App;

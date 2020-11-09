8f3b2175ba15bc2a18cb3e4e4cbc6186f078d0cc
Renato Silva <renatosdev@gmail.com>
2020-11-09 15:57:17 -0300

    Requisito 01 e 02

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+// import MovieCard from './components/MovieCard';
+// import MovieList from './components/MovieList';
+// import Rating from './components/Rating';
 
-function App() {
-  return (
-    <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+      <Header />
     </div>
-  );
+    );
+  }
 }
-
 export default App;

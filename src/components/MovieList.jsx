af2619e03155bb9fedc0a967fe02148bb448ae42
Pedro Marques <pedromarques1011@gmail.com>
2020-11-09 16:27:21 -0300

    codeclimate2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -17,10 +17,35 @@ class MovieList extends React.Component {
 }
 
 MovieList.propTypes = {
-  movies: PropTypes.array,
+  movies: PropTypes.arrayOf(PropTypes.object),
 };
 MovieCard.defaultProps = {
-  movies: [],
+  movies: [
+    {
+      title: 'Kingsglaive',
+      subtitle: 'Final Fantasy XV',
+      storyline:
+        "King Regis, who oversees the land of Lucis, commands his army of soldiers to protect the kingdom from the Niflheim empire's plans to steal the sacred crystal.",
+      rating: 4.5,
+      imagePath: 'images/Kingsglaive_Final_Fantasy_XV.jpg',
+    },
+    {
+      title: 'Final Fantasy',
+      subtitle: 'Spirits Within',
+      storyline:
+        'A scientist makes a last stand on Earth with the help of a ragtag team of soldiers against an invasion of alien phantoms.',
+      rating: 4.5,
+      imagePath: 'images/Final_Fantasy_Spirits_Within.jpg',
+    },
+    {
+      title: 'Ghost In The Shell',
+      subtitle: 'Ghost In The Shell',
+      storyline:
+        'A hacker known as the Puppet Master is hunted by a female cyborg cop and her partner. This film is a revised version of Ghost in the Shell (1995).',
+      rating: 5,
+      imagePath: 'images/Ghost_In_The_Shell_2_0.jpg',
+    },
+  ],
 };
 
 export default MovieList;

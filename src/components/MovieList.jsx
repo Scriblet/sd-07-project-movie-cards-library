057f65034b935c039a0b162fbedb0f3181e2f43f
danielmlduarte <danielduarte.tryber@gmail.com>
2020-11-09 15:06:30 -0300

    Resolvendo Issues Code Climate

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -14,6 +14,6 @@ class MovieList extends React.Component {
   }
 }
 
-MovieList.propTypes = { movies: PropTypes.arrayOf({}) };
+MovieList.propTypes = { movies: PropTypes.arrayOf({}).isRequired };
 
 export default MovieList;

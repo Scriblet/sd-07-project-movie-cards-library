9d6eb1da6d296923dc1c6df7b72b4e530177538b
Lorena Góes <goesloren@gmail.com>
2020-11-09 16:26:31 -0300

    Testando componente MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+// import movies from '../data';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movieList">
+        <p>{movies}</p>
+      </div>
+    );
+  }
+}
+
+export default MovieList;

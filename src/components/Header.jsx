fa66fd572a674affcdb1bc85236e4a31730a90fe
MarceloBorges <melombm@hotmail.com>
2020-11-09 16:42:56 -0500

    exercise 1 e 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,14 @@
 // implement Header component here
+import React from 'react'
+
+const Header = (props) =>{
+    
+    return (
+        <header>
+            <h1>Movie Cards Library</h1>
+        </header>
+    )
+    
+}
+
+export default Header
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,10 @@
 // implement MovieList component here
+import React from 'react'
+import moveis from './data'
+
+const MovieList = (props) =>{
+    const {moveis} = props
+    return(
+
+    )
+}

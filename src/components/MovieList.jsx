46563f0b56568748faaeed34096db74aae72a843
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-11-09 13:40:32 -0300

    First commit

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const moviesList = this.props.movies;
+    return (
+      <div className='movie-list'>
+        {moviesList.map(movies => <MovieCard key={movies.title} movies={movies}/>
+        )}
+      </div>
+    )
+  }
+}
+
+export default MovieList;

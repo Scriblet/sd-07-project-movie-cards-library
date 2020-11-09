d0427691a87aa311d58686d7e246b4784e3f2126
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-11-09 15:23:56 -0300

    Finalizado projeto sem estilizar

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
-import Rating from './Rating'
+import React, { Component } from 'react';
+import Rating from './Rating';
 class MovieCard extends Component {
-    render(){
-        const { title, subtitle, storyline, imagePath, rating } = this.props.movie
-        return (
-            <div>
-                <img src={imagePath} />
-                <h4>{title}</h4>
-                <h5>{subtitle}</h5>
-                <p>{storyline}</p>
-                <Rating rating={rating} />
-            </div>
-        )
-    }
+	render() {
+		const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+		return (
+			<div>
+				<img src={imagePath} />
+				<h4>{title}</h4>
+				<h5>{subtitle}</h5>
+				<p>{storyline}</p>
+				<Rating rating={rating} />
+			</div>
+		);
+	}
 }
 
-export default MovieCard
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
-import MovieCard from './MovieCard'
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
 class MovieList extends Component {
-    render(){
-        return (
-            this.props.movies.map(movie => <MovieCard movie={movie} key={movie.title} />)
-        )
-    }
+	render() {
+		return this.props.movies.map(movie => (
+			<MovieCard movie={movie} key={movie.title} />
+		));
+	}
 }
 
-export default MovieList
+export default MovieList;

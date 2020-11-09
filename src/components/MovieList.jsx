af28485ed7bb159f9651dea7c282c0aae3242b8d
Julio Kauer <juliokauer@gmail.com>
2020-11-09 18:13:19 -0300

    Requisito 4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import MovieCard from './MovieCard';
 import movies from '../data';
 
 class MovieList extends Component {
   render() {
     return (
-      <div>
-        {movies.map((movie) => <div>{movie.title}</div>)}
+      <div className="movie-list">
+        {movies.map((movie) => <MovieCard movie={movie} />)}
       </div>
     );
   }

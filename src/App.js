ff1a14a97164da7b1ba867381f1058e40f853df5
Andre Menezes <dehhmenezes87@gmail.com>
2020-11-09 18:32:46 -0300

    Codeclimate fixed #1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Header from './components/Header';
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-  return(
+  return (
     <div className="App">
       <Header />
     </div>
   )
-}
+};
 
 export default App;

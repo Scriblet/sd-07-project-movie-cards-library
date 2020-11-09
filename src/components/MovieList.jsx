58b3c457c604b27ed1f97573fcdb9078bf2f1a1e
mustang-roy <lfsmariz@gmail.com>
2020-11-09 14:28:14 -0300

    First Commit

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,30 @@
 // implement MovieList component here
+import React from 'react'
+import MovieCard from './MovieCard'
+import PropTypes from 'prop-types'
+
+class MovieList extends React.Component{
+  
+  render() {
+    return (
+      <div className='movie-list'>
+      {this.props.movies.map((movCard) =>(
+        <MovieCard key={movCard.title} movie={movCard}/>
+       ))}
+      </div>
+    ) 
+    
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired).isRequired
+};
+
+export default MovieList
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,21 @@
 // implement Rating component here
+import React from 'react'
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component{
+  
+  render() {
+    const {rating = 0} = this.props
+    return (
+      <p className='rating' >
+        {rating}
+      </p>
+    )  
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+
+export default Rating

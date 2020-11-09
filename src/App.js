d3bf56f0267dfb1eb80bed20b2ed42d9f8bf2849
Filipi Firmino <contatoic3web@gmail.com>
2020-11-09 17:38:28 -0300

    CC 2

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
-import DataVideo from './data'; 
+import DataVideo from './data';
 import MovieCard from './components/MovieCard';
 
 
 function App() {
   return (
     <div className = "App">
-      <Header title = { <h1 className = 'page-title'> Movies Card Library </h1> }/>
-      <MovieList movies = { DataVideo.map((acc, index) => <MovieCard movies = { acc }
-      key = { index }/>)}/>
+      <Header title = {<h1 className = "page-title"> Movies Card Library </h1>}/>
+      <MovieList movies = {
+        DataVideo.map((acc) => <MovieCard movies = {acc} key = {acc.title}/>)
+        }/>
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,22 +1,28 @@
 // implement MovieCard component here
 import React from 'react';
-import Rating from './Rating'
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
-    render() {
-        const {imagePath, title, subtitle, storyline,rating} = this.props.movies
-      return <div className='movie-card'>
-          <img className = 'movie-card-image' src={imagePath} alt ={this.props.alt}/>
-          <div className ='movie-card-body'>
-            <h3 className ='movie-card-title'>{title}</h3>
-            <h4 className ='movie-card-subtitle'>{subtitle}</h4>
-            <p className = 'movie-card-storyline'>{storyline}</p>
-          </div>
-          <div className =''>
-            <Rating rating = {rating}/>
-          </div>
+  render() {
+    const { imagePath, title, subtitle, storyline, rating } = this.props.movies;
+    return (
+      <div className="movie-card">
+        <img
+          className="movie-card-image"
+          src={imagePath}
+          alt={this.props.alt}
+        />
+        <div className="movie-card-body">
+          <h3 className="movie-card-title">{title}</h3>
+          <h4 className="movie-card-subtitle">{subtitle}</h4>
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
+        <div className="movie-card-rating">
+          <Rating rating={rating} />
+        </div>
       </div>
-    }
+    );
+  }
 }
 
 export default MovieCard;

0e83bcc412a868f05a5eae22dc157f6f00d30a7e
brendalima <brenda.c.lima@poli.ufrj.br>
2020-11-09 19:12:37 -0300

    adding propTypes check to MovieList.jsx

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,15 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieList extends React.Component {
   render() {
     return <div>{ this.props.movies }</div>;
   }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.array
 }
 
 export default MovieList;

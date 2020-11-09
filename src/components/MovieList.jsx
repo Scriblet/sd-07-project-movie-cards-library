99eb751fff947c308b78dca4a4f4683661878889
Pedro Marques <pedromarques1011@gmail.com>
2020-11-09 15:22:37 -0300

    moviecard, movielist, rating

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,23 @@
 // implement MovieList component here
+import React from "react";
+import PropTypes from "prop-types";
+
+import MovieCard from "./MovieCard";
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div className="movie-list">
+        {this.props.movies.map((movie) => {
+          return <MovieCard key={movie.title} movie={movie} />;
+        })}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.array,
+};
+
+export default MovieList;

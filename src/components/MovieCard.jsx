91ada7ba1c2dd348bb86200bff4be20623955f18
Emanuelle Brasil <emanuelle.rbl@gmail.com>
2020-11-09 17:54:48 -0300

    criação de componentes MovieCard e MovieList

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
 // implement MovieCard component here
+import React from 'react';
+
+const MovieCard = (props) => {
+  const { movie } = props;
+  return (
+    <div className="movie-card">
+      <img src={movie.imagePath} alt="movie"/>
+    </div>
+  );
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
 // implement MovieList component here
+// Este componente representará toda a área com os cartões de filmes. 
+// MovieList deve receber uma prop movies, que é um array de objetos com informações de um filme.
+import React from 'react';
+import MovieCard from './MovieCard';
+
+
+const MovieList = (props) => {
+  const { movies } = props;
+  return (
+    <div className="movie-list">
+      {movies.map(movie => <MovieCard key={movie.title} data={movie} />)}
+    </div>
+  );
+};
+
+export default MovieList;

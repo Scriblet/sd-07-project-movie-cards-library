072ee5fb1d701899978a8ce0c03daa3226123e77
renatomak <renato.mark.silva@gmail.com>
2020-11-09 17:14:50 -0300

    requisito 3-2-CC

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,46 +7,15 @@ class MovieList extends Component {
     return (
       <main>
         <div>
-            <MovieCard 
-            title={this.props.movies[0].title} 
-            subtitle={this.props.movies[0].subtitle} 
-            storyline={this.props.movies[0].storyline} 
-            rating={this.props.movies[0].rating} 
-            imagePath={this.props.movies[0].imagePath}/>
-
-            <MovieCard 
-            title={this.props.movies[1].title} 
-            subtitle={this.props.movies[1].subtitle} 
-            storyline={this.props.movies[1].storyline}  
-            rating={this.props.movies[1].rating} 
-            imagePath={this.props.movies[1].imagePath}/>
-
-            <MovieCard 
-            title={this.props.movies[2].title} 
-            subtitle={this.props.movies[2].subtitle} 
-            storyline={this.props.movies[2].storyline} 
-            rating={this.props.movies[2].rating} 
-            imagePath={this.props.movies[2].imagePath}/>
-
-            <MovieCard 
-            title={this.props.movies[3].title} 
-            subtitle={this.props.movies[3].subtitle} 
-            storyline={this.props.movies[3].storyline} 
-            rating={this.props.movies[3].rating} 
-            imagePath={this.props.movies[3].imagePath}/>
-
-            <MovieCard 
-            title={this.props.movies[4].title} 
-            subtitle={this.props.movies[4].subtitle} 
-            storyline={this.props.movies[4].storyline} 
-            rating={this.props.movies[4].rating} 
-            imagePath={this.props.movies[4].imagePath}/>
-            
+            <MovieCard title={this.props.movies[0].title} subtitle={this.props.movies[0].subtitle} storyline={this.props.movies[0].storyline} rating={this.props.movies[0].rating} imagePath={this.props.movies[0].imagePath}/>
+            <MovieCard title={this.props.movies[1].title} subtitle={this.props.movies[1].subtitle} imagePath={this.props.movies[1].imagePath}/>
+            <MovieCard title={this.props.movies[2].title} subtitle={this.props.movies[2].subtitle} storyline={this.props.movies[2].storyline} rating={this.props.movies[2].rating} imagePath={this.props.movies[2].imagePath}/>
+            <MovieCard title={this.props.movies[3].title} subtitle={this.props.movies[3].subtitle} storyline={this.props.movies[3].storyline} rating={this.props.movies[3].rating} imagePath={this.props.movies[3].imagePath}/>
+            <MovieCard title={this.props.movies[4].title} subtitle={this.props.movies[4].subtitle} storyline={this.props.movies[4].storyline} rating={this.props.movies[4].rating} imagePath={this.props.movies[4].imagePath}/> 
         </div>
       </main>
     );
   }
 }
-
 
 export default MovieList;

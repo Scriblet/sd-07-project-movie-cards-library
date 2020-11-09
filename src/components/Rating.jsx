9475a20c1aafa21fc3b92af0d02e3be65b53a664
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-11-09 19:32:03 -0300

    testando todos os requisitos

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,19 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const rated = this.props.rating;
+    return (
+      <div className="movie-card-rating">
+        <div className="rating">{rated}</div>
+      </div>
+    );
+  }
+}
+
+export default Rating;
+
+Rating.propTypes = { rating: PropTypes.number };
+
+Rating.defaultProps = { rating: 0 };

4047b6bbc823532de168f7e29a6e8201cd1abdec
Renato Silva <renatosdev@gmail.com>
2020-11-09 16:12:24 -0300

    Requisito 01 e 02

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-// import MovieCard from './components/MovieCard';
+import MovieCard from './components/MovieCard';
 // import MovieList from './components/MovieList';
 // import Rating from './components/Rating';
 
@@ -10,7 +10,7 @@ class App extends React.Component {
     return (
       <div className="App">
       <Header />
-    </div>
+      </div>
     );
   }
 }

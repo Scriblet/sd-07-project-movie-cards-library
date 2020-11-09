cf1bb0539d75006d0d9bf881027cbd1107a21f05
marcelo ivan <sagacello@hotmail.com>
2020-11-09 15:41:54 -0300

    primeiro-commit

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,29 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render () {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movies; 
+    return (
+      <div>
+          <img src={imagePath} alt={`${title} sprite`} ></img>
+          <h4> {title} </h4>
+          <h5> {subtitle} </h5>
+          <p> {storyline}</p>
+        <span>Rating {rating}</span>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+    movies: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline:PropTypes.string,
+    image: PropTypes.string,
+    rating: PropTypes.number,
+  })
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+// o map é feito em pokemons , mais pokemons so vai ser passado no App.js
+class MovieList extends React.Component {
+    render() {
+        return (
+            <div >
+                {this.props.movies.map(filme => <MovieCard key={filme.title} movies={filme} />)}   
+            </div>
+        );
+    }
+}
+
+export default MovieList;

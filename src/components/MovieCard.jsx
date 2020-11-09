b40353e6bde6ce473a1add839412034337b509e1
Emerson Moonlit <emoonlit@pop-os.localdomain>
2020-11-09 18:07:27 -0300

    CC Ajust

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
-    render() {
-        const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
-        return (
-            <div className='movie-card'>
-                <img src={imagePath} alt={{title} + " " + {subtitle}}/>
-                <div>
-                    <h4>{title}</h4>
-                    <h5>{subtitle}</h5>
-                    <p>{storyline}</p>
-                </div>
-                <div>
-                    <Rating rating={rating}/>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    return (
+      <div className="movie-card">
+        <img src={imagePath} alt={`${title} ${subtitle}`} />
+        <div>
+          <h4>{title}</h4>
+          <h5>{subtitle}</h5>
+          <p>{storyline}</p>
+        </div>
+        <div>
+          <Rating rating={rating} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-    render() {
-      const { movies } = this.props;
-      return (
-        <section className='movie-list'>
-          {movies.map((movie) => (<MovieCard  key={movie.title} movie={movie} />))}
-        </section>
-      );
-    }
+  render() {
+    const { movies } = this.props;
+    return (
+      <section className="movie-list">
+        {movies.map((movie) => (<MovieCard key={movie.title} movie={movie} />))}
+      </section>
+    );
   }
-  
-  export default MovieList;
+}
+
+export default MovieList;

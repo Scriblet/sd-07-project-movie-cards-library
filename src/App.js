a7424904ebdc50a290bb9c9b173b4fcc74ebbfc4
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-11-09 18:28:42 -0300

    Criando componente movieList e MovieCard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import data from './data';
 import Header from './components/Header';
+import MoviesList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MoviesList movies={data} />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <div className="movie-card-body">
+        <img src={movie.imagePath} alt="imagem" />
+      </div>
+    );
+  }
+}
+
+export default MovieCard;

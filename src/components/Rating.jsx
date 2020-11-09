7b5229d06344583dcb23ccaa15764ec373d5a326
moraisgabri <gabriel8azevedo@gmail.com>
2020-11-09 16:14:07 -0300

    comitaço

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,13 @@
-// implement Rating component here
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+    return (
+      <div className="rating">
+        <em>{this.props.rating}</em>
+      </div>
+    );
+  }
+}
+
+export default Rating;

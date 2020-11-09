182cae8693bf3d3b0dbab2bda52562820e7b68ce
Emerson Moonlit <emoonlit@pop-os.localdomain>
2020-11-09 16:59:58 -0300

    Movie List

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header.jsx';
+import MovieList from './components/MovieList.jsx';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <MovieList />
       </div>
     );
   }

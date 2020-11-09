aad4248596bfae036faac576d3e630b538af12ae
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-11-09 15:32:26 -0300

    CC

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-import PropTypes from 'prop-types'
+
 
 class MovieList extends Component {
   render() {

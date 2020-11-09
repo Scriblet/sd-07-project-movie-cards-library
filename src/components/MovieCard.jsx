ab4aa888b5a27bd2e5ecaf54f49ea665db0e46d4
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-11-09 18:30:04 -0300

    diminuindo issues

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,8 +10,8 @@ class MovieCard extends React.Component {
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
-        <img alt='imagem do filme' src={imagePath}></img>
-        <Rating rating={rating}/>
+        <img alt='imagem do filme' src={imagePath} />
+        <Rating rating={rating} />
       </div>
     );
   }

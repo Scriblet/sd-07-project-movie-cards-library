2d7e2bea98eafc771c64bab939742abe854c8a6b
Lucas Andrade <lafdesouza2002@outlook.com>
2020-11-09 16:38:47 -0300

    Cumprindo mais alguns requisitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,21 @@
 // implement MovieCard component here
-// implement Header component here
 import React from 'react';
+import Rating from './Rating.jsx'
 
 class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props.key;
+    return (
+        <div>
+            <img src={movie.imagePath} alt=""></img>
+            <h4>{movie.title}</h4>
+            <h5>{movie.subtitle}</h5>
+            <p>{movie.storyline}</p> 
+            <Rating rating={movie.rating}/>
+        </div>
+
+    )
+  }
 }
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+// implement Header component here
+import React from 'react';
+import MovieCard from './MovieCard.jsx'
+
+class MovieList extends React.Component {
+  render() {
+    return (
+        this.props.movies.map(movie => <MovieCard key={movie.name}/>)
+    );
+  }
+}
+
+export default MovieList;

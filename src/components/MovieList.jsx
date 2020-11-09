e46fcbe128a0012684b77aa4cac1017b5e55c417
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-11-09 15:13:04 -0300

    CC

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import MovieCard from './MovieCard';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+  }  
+
   render() {
     const movies = this.props.movies;
 

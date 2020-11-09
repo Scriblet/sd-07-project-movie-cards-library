5cf7eb197d8a4edfad2f3af4a74cc8b0cb002ac9
Julio Kauer <juliokauer@gmail.com>
2020-11-09 19:16:57 -0300

    Requisito 11

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
-    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    const { movie } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
       <div className="movie-card">
-        <img className="movie-card-image" src={imagePath} alt={title} />:
+        <img className="movie-card-image" src={imagePath} alt={title} />
         <div className="movie-card-body">
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
         </div>
-        <div className="movie-card-rating">
-          <div className="rating">{rating}</div>
-        </div>
+        <Rating rating={rating} />
       </div>
     );
   }

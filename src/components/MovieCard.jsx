dca47978e9096c8dfc769541878d5de68b407370
cezar88augusto <cezar88augusto@gmail.com>
2020-11-09 17:02:20 -0300

    Projeto: Movie Cards Library

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
 // implement MovieCard component here
+import React from 'react';
+import Rating from './Rating'
+
+class MovieCard extends React.Component {
+    render() {
+        const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+        return (
+            <div >
+                <h4>{title}</h4>
+                <h5>{subtitle}</h5>
+                <p>{storyline}</p>
+                <img src={imagePath} alt={`${title}`} />
+                <div>
+                    <Rating rating={rating}></Rating>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default MovieCard;

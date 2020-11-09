df81e4f570ed84913fbe1050bdfdd02cb85bbc14
Strongreen <strongreen@live.com>
2020-11-09 19:35:57 -0300

    correçao CC

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,16 +6,16 @@ class MovieCard extends React.Component {
   render() {
     return Movies.map((movies) => {
       return (
-        <div className='movie-card'>
-          <h4 className='movie-card-title'>{movies.title}</h4>
-          <h5 className='movie-card-subtitle'>{movies.subtitle}</h5>
+        <div className="movie-card">
+          <h4 className="movie-card-title">{movies.title}</h4>
+          <h5 className="movie-card-subtitle">{movies.subtitle}</h5>
           <img
-            className='movie-card-image'
+            className="movie-card-image"
             src={movies.imagePath}
-            alt='Imagem'
+            alt="Imagem"
           />
-          <p className='movie-card-storyline'>{movies.storyline}</p>
-          <p className='movie-card-rating'>
+          <p className="movie-card-storyline">{movies.storyline}</p>
+          <p className="movie-card-rating">
             <Rating />
           </p>
         </div>

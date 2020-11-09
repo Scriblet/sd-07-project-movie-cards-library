b8476d8d6ebb8626b2ddcbc68aa40446434a671b
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-11-09 16:07:17 -0300

    Codeclimate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Rating from './Rating';
+
 class MovieCard extends Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <div>
-        <img src={imagePath} />
+        <img src={imagePath} alt={`${title} cover`} />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>

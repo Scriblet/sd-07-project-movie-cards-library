6215c979061c0f8f89553d3b837deacd6470759d
Filipi Firmino <contatoic3web@gmail.com>
2020-11-09 16:32:23 -0300

    Card Movie project finish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
 // implement Header component here
+
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+    return <div className = 'movie-card-header'>{this.props.title}</div>
+    }
+}
+
+export default Header;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
 // implement MovieCard component here
+import React from 'react';
+import Rating from './Rating'
+
+class MovieCard extends React.Component {
+    render() {
+        const {imagePath, title, subtitle, storyline,rating} = this.props.movies
+      return <div className='movie-card' key = {this.props.index}>
+          <img className = 'movie-card-image' src={imagePath} alt ={this.props.alt}/>
+          <div className ='movie-card-body'>
+            <h3 className ='movie-card-title'>{title}</h3>
+            <h4 className ='movie-card-subtitle'>{subtitle}</h4>
+            <p className = 'movie-card-storyline'>{storyline}</p>
+          </div>
+          <div className =''>
+            <Rating rating = {rating}/>
+          </div>
+      </div>
+    }
+}
+
+export default MovieCard;

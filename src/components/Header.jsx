d8f23937385b7732dae49a649a45d661cd738414
Lucas Andrade <lafdesouza2002@outlook.com>
2020-11-09 15:43:39 -0300

    Corrigindo erro

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-      return (
+    return (
         <header>
-        <h1>
-            Movie Cards Library
-        </h1>
-    </header> 
-      )
+            <h1>
+                Movie Cards Library
+            </h1>
+        </header>
+      );
   }
-};
+}
 
 export default Header;

74b7781d98c0d70311f78ce71a2cc43bd791fb05
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-11-09 16:39:44 -0300

    code climate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import propTypes from 'prop-types';
+
 
 class Header extends React.Component {
   render() {
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import MovieCard from './MovieCard';
+import propTypes from 'prop-types';
 
 class MovieList extends React.Component {
   render() {
-    const movies = this.props.movies;
+    const { movies } = this.props;
     return (
       <div>
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
@@ -13,3 +14,16 @@ class MovieList extends React.Component {
 }
 
 export default MovieList;
+
+MovieList.propTypes = {
+  movies: propTypes.arrayOf(
+    propTypes.shape({
+      title: propTypes.string,
+      subtitle: propTypes.string,
+      storyline: propTypes.string,
+      rating: propTypes.number,
+      imagePath: propTypes.string
+ 
+    })
+  )
+}

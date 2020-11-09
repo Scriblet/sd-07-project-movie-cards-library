2410dbf05c03a70058e41ec80267599605745124
vini-lima <vini20.henrique@gmail.com>
2020-11-09 14:20:53 -0300

    Iniciando o Projeto: Booooooooooooooooraaaaa! vini

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,21 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies: movieListArray } = this.props;
+
+    return (
+      <div>
+        {movieListArray.map((movie) => (
+          <MovieCard key={movie.title} movie={movie} />
+        ))}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
+
+export default MovieList;

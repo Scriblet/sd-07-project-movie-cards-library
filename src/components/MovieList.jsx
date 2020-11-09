b36bd11ca67426641a5e2833ee04434caf176094
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-11-09 16:32:00 -0300

    concertando erros do codeclimate do requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,13 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieList extends React.Component {
-    render() {
-        const movies = this.props.movies;
-
-        return;
-    }
+  render() {
+    return this.props.movies;
+  }
 }
 
 export default MovieList;
+
+MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };

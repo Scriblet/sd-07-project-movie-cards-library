e959f9fe2a4c91050ef51e9f0bd2f0774c05897a
Jr3564 <rodolfojr94@gmail.com>
2020-11-09 19:30:53 -0300

    codeclimate MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,20 +3,20 @@ import Rating from './Rating.jsx';
 
 export default class MovieCard extends React.Component {
   getMovieElement() {
-    const {title, imagePath, subtitle, storyline, rating} = this.props.children;
+    const { title, imagePath, subtitle, storyline, rating } = this.props.children;
     return (
-    <div className='movie-card'>
-      <div key={title} className='movie-card-budy'>
-        <img src={imagePath} className='movie-card-image'/>
-        <h4 className='movie-card-title'>{title}</h4>
-        <h5 className='movie-card-subtitle'>{subtitle}</h5>
-        <p className='movie-card-storyline'>{storyline}</p>
+      <div className="movie-card">
+        <div key={ title } className="movie-card-budy">
+          <img src={ imagePath } className="movie-card-image"/>
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+        </div>
+        <Rating rating={ rating } className="rating"/>
       </div>
-      <Rating rating={rating} className='rating'/>
-    </div>
-    )
+    );
   }
   render() {
-    return this.getMovieElement()
+    return this.getMovieElement();
   }
-};
+}

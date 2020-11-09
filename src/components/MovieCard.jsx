f0f1b848f81cf81259d2c521f913513dadf8ba8c
Samanta Below <samanta.below@gmail.com>
2020-11-09 19:12:31 -0300

    CSS finalizado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,12 +6,14 @@ class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
-      <div>
-        <img src={movie.imagePath} alt="movie flyer" />
-        <h4>{movie.title}</h4>
-        <h5>{movie.subtitle}</h5>
-        <p>{movie.storyline}</p>
-        <Rating rating={movie.rating} />
+      <div className="movie-card">
+        <img className="movie-card-image" src={movie.imagePath} alt="movie flyer" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{movie.title}</h4>
+          <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
+          <p className="movie-card-storyline">{movie.storyline}</p>
+        </div>
+        <Rating className="movie-card-rating" rating={movie.rating} />
       </div>
     );
   }
@@ -24,7 +26,7 @@ MovieCard.propTypes = {
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
-    rating: PropTypes.num.isRequired,
+    rating: PropTypes.number.isRequired,
     image: PropTypes.string.isRequired,
   }).isRequired,
 };

6d57ca88be806911f82dcb5c771de353a7d0d977
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-11-09 16:08:41 -0300

    code climate

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,14 +3,13 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props;
-    console.log(movies)
-    return(
+    const movies = this.props.movies;
+    return (
       <div>
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
-    )
-  };
-};
+    );
+  }
+}
 
 export default MovieList;

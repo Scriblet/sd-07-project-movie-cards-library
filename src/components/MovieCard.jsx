d18ec9e5bde178aefb8082b3120c8609f173bf03
vanderson-henrique <vhbenedito@gmail.com>
2020-11-09 15:27:57 -0300

    Requisitos 1 a 16

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ import Rating from "./Rating";
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
-
+    
     return (
       <div>
         <img src={imagePath} />

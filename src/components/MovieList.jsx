53cc9744361b7b903033f600e7cf6eb9d87a5037
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-11-09 16:47:00 -0300

    requisito 4 | renderizando movieCard para cada objeto da lita

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,17 @@
 // implement MovieList component here
 import React from 'react';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return this.props.movies;
+    const movies = this.props.movies;
+
+    return (
+        movies.map((movie) => {
+            return <MovieCard />
+        })
+    );
   }
 }
 

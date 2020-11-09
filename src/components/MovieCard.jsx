ecd4f27a207a6c24b84cc7611222d4585449d11b
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-11-09 19:55:32 -0300

    Criando componente Rating e renderizando

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { imagePath, title, subtitle, storyline } = movie;
+    const { imagePath, title, subtitle, storyline, rating } = movie;
     return (
       <div className="movie-card">
         <img className="movie-card-image" src={imagePath} alt="imagem" />
@@ -12,6 +13,9 @@ class MovieCard extends React.Component {
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline ">{storyline}</p>
+        </div>
+        <div className="movie-card-rating ">
+          <Rating rating={rating} />
         </div>
       </div>
     );

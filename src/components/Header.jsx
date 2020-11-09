3ddb4fc6e73612583f2aa050b5018c43ad142e93
sttefanieMaciel <macielsttefanie@gmail.com>
2020-11-09 19:40:35 -0300

    Tentando enviar esse commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,9 @@
-// implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+export default Header;

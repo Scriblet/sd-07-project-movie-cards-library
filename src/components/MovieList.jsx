5822fb7faeab9851be33149da938a43a27315ede
CleberFontinele <cleberfontinele2387@gmail.com>
2020-11-09 16:44:56 -0300

    Crie um componente chamado MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+import data from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        { data}
+      </div>
+    );
+  }
+}
+
+export default MovieList;

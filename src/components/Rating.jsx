12c186db446b870b4cc9c8143a06984edbec502d
Vanessa Oliveira <vanessanaara@gmail.com>
2020-11-09 19:12:01 -0300

    Definindo as propTypes

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    return <span className="rating">{this.props.rating}</span>
+    return <span className="rating">{this.props.rating}</span>;
   }
 }
+
+Rating.propTypes = { rating: PropTypes.number };
+
+Rating.defaultProps = { rating: 4 };
 
 export default Rating;

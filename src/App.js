c316355193890b7d19c82a6f52cb0db74fb8375b
jvitorof <j.vitoroflores@gmail.com>
2020-11-09 16:45:26 -0300

    First commit: Req#1 Header show! :rocket:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+// import MovieList from './components/MovieList';
+// import MovieCard from './components/MovieCard';
+// import Rating from './components/Rating';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      {<Header />}
+      🚀Trybe
     </div>
   );
 }

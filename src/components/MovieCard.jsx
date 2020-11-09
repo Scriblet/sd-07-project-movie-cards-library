2a0ff3bcec2a5f54ebcad92562d34d80a593c14b
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-11-09 16:00:51 -0300

    adicionando redering movie cart image

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends Component {
   render() {
-    return (<div>Movie</div>);
+    const { title, imagePath } = this.props.movie;
+
+    return (
+      <div>
+        <img src={imagePath} alt={title} />
+      </div>
+    );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }),
+};
 
 export default MovieCard;

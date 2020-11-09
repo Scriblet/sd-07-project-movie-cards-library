4c21c3c7ef0d6f28f5b5f30ed81a568da7784eb8
Eduardo Souza <dado.eletrons@gmail.com>
2020-11-09 13:46:11 -0300

    Edu_Souza--sd-07_movie_cards_library

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,21 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+// prettier-ignore
+export default class MovieList extends React.Component {
+  render() {
+    return (
+      <main>
+        <div className="movie-list">
+          {this.props.movies.map((movie) => (
+            <MovieCard key={movie.title} movie={movie} />
+            ))}
+        </div>
+      </main>
+    );
+  }
+}
+
+// prettier-ignore
+MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,17 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Rating extends React.Component {
+  render() {
+    return (
+      <div className="movie-card-rating">
+        <p>
+          Rating <span className="rating">{this.props.rating}</span>
+        </p>
+      </div>
+    );
+  }
+}
+
+// prettier-ignore
+Rating.propTypes = { rating: PropTypes.number.isRequired };

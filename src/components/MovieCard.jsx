a5ae4465618908d682ecf0cdf90bd967232cd26b
Carol Bezerra <carol.arbe@gmail.com>
2020-11-09 18:59:24 -0300

    Requisitos do 3 ao 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,20 @@
 // implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { imagePath, title, subtitle, storyline, rating } = this.props.movie;
+    return(
+      <div>
+        <img src={imagePath} alt='Nice Movie'></img>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating rating={rating}/>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+// import React from 'react';
+// import MovieCard from './MovieCard';
+
+// class MovieList extends React.Component {
+//   render() {
+//     const { title } = this.props.movies;
+//     return(
+//       {movies.map(element => <MovieCard key={element.title} />)}
+//     );
+//   }
+// }
+
+// export default MovieList;

7f69e2dbe9aa48f1a9d2cc9024c9dcf9e6be2c44
Pablo <pablopessanh@gmail>
2020-11-09 14:59:48 -0300

    requirements 7th, 8th, 9th and 10th done

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,11 +4,16 @@ import React, { Component } from 'react';
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
-    const { title } = movie;
+    const { title, subtitle, storyline, imagePath } = movie;
 
     return (
       <div className='movie-card' key={title}>
-        <span>Olá</span>
+          <img  className='movie-card-image' src={imagePath} alt={title}/>
+        <div className="movie-card-body">
+          <h4 className='movie-card-title'>{title}</h4>
+          <h5 className='movie-card-subtitle'>{subtitle}</h5>
+          <p className='movie-card-storyline'>{storyline}</p>
+        </div>
       </div>
     );
   };

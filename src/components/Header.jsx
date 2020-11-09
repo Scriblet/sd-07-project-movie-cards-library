5cdee1776c4f21f42191229494ccdf6ecde9d79e
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-11-09 14:39:45 -0300

    Commit inicial.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,20 @@
-// implement Header component here
+import React from 'react';
+import propTypes from 'prop-types';
+import '../styles/components/header.css';
+
+class Header extends React.Component {
+  render() {
+    const { title } = this.props;
+    return (
+      <header className="header">
+        <h1>{title}</h1>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = { title: propTypes.string };
+
+Header.defaultProps = { title: 'Movie Cards Library' };
+
+export default Header;

5a00087e96a3c27912726975e3aa18ac774bf043
Bruno Nogueira <nugobruno@gmail.com>
2020-11-10 01:42:23 -0300

    Requisito 2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,26 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props;
+    return (
+      <div className="movie-card-body">
+        <img className="movie-card-image" src={imagePath} alt="movie thumbnail" />
+        <h4 className="movie-card-title">{title}</h4>
+        <span className="movie-card-subtitle">{subtitle}</span>
+        <span className="movie-card-storyline">{storyline}</span>
+        <span className="rating">{rating}</span>
+      </div>);
+  }
+}
+
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  imagePath: PropTypes.string.isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,52 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+const movies = [
+  {
+    title: 'Movie Title 1',
+    subtitle: 'Movie Subtitle 1',
+    storyline: 'Movie Storyline 1',
+    rating: 4.5,
+    imagePath: 'images/movie_1',
+  },
+  {
+    title: 'Movie Title 2',
+    subtitle: 'Movie Subtitle 2',
+    storyline: 'Movie Storyline 2',
+    rating: 4.5,
+    imagePath: 'images/movie_2',
+  },
+  {
+    title: 'Movie Title 3',
+    subtitle: 'Movie Subtitle 3',
+    storyline: 'Movie Storyline 3',
+    rating: 3,
+    imagePath: 'images/movie_3',
+  },
+];
+
+class MovieList extends Component {
+  render() {
+    return (
+      movies.map((movie) => {
+        const setKey = movie.title.toString();
+        return (
+          <section className="movie-list">
+            <div className="movie-card">
+              <MovieCard
+                key={setKey}
+                title={movie.title}
+                subtitle={movie.subtitle}
+                storyline={movie.storyline}
+                rating={movie.rating}
+                imagePath={movie.imagePath}
+              />
+            </div>
+          </section>
+        );
+      })
+    );
+  }
+}
+
+export default MovieList;

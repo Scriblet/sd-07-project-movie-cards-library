7c6e62a6a83bdbf47881077af35d746ed4b55528
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2020-11-09 21:19:54 -0300

    Terceiro, Quarto e Quinto requisito

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <p>Test</p>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const movies = this.props.movies;
+    return (
+      <div>
+        {movies.map((movie) => <MovieCard key={movie.title} />)}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = { movies: PropTypes.element.isRequired };
+
+export default MovieList;

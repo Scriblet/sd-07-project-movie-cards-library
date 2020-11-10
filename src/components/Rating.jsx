da3294cbb1f17b2d03903411846a13efdc056d8b
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-11-10 04:42:21 -0300

    Ajustando cc - 10/11/2020

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,11 +1,28 @@
 // implement Rating component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../App.css';
 
 class Rating extends React.Component {
   render() {
-    return <div className="rating">{this.props.rating}</div>;
+    const rank = this.props;
+    return (
+      <div className="rating">
+        <p>{rank.rating}</p>
+      </div>
+    );
   }
+}
+
+Rating.propTypes = {
+  rank: PropTypes.shape({
+    rating: PropTypes.string.isRequired,
+  })
+};
+
+Rating.defaultProps = {
+  rank: {},
+  rating: 'number',
 }
 
 export default Rating;

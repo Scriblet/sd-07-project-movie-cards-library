b3918c5a8bdaf325d245285509c922e8a7bd87e5
sttefanieMaciel <macielsttefanie@gmail.com>
2020-11-10 17:41:33 -0300

    Terceiro  Requisito

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 class MovieList extends React.Component {
   render() {
     return (
-        <div className="movie-list">
-        </div>
+      <div className="movie-list">
+        <h1>Movie</h1>
+      </div>
     );
   }
 }

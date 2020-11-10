0c04de8a98f95a874267040793a8b626ac9b78ef
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-11-10 11:09:44 -0300

    Ajusting code climate

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -11,6 +11,6 @@ class Rating extends React.Component {
   }
 }
 
-Rating.PropTypes = { rating: PropTypes.number.isRequired };
+Rating.propTypes = { rating: PropTypes.number.isRequired };
 
 export default Rating;

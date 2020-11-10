7dc26dee679a2ccfd50d995cc8d6b5c4eb39c81d
lisvalver <lisvalver.atte@gmail.com>
2020-11-09 21:40:01 -0300

    css added || finalizing project || today is a big day! 🥳🚀

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,7 +6,7 @@ import MovieCard from './MovieCard';
 class MovieList extends React.Component {
   render() {
     return (
-      <div>
+      <div className="movie-list">
         {this.props.movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)};
       </div>
     );

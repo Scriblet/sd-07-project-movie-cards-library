099c22f4191b3adf7e50ad9f46085779096febcc
Raphael Caputo <faelelf2@gmail.com>
2020-11-10 02:14:25 -0300

    Em busca dos 100%

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 
 class MovieCard extends Component {
   render() {
-    const { title, subtitle, imagePath, storyLine } = this.props.movie;
+    const { title, subtitle, imagePath, storyLine, rating } = this.props.movie;
     return (
       <div className="movie-card">
         <img className="movie-card-image" src={imagePath} alt={`${title} ${subtitle}`} />
@@ -12,6 +13,7 @@ class MovieCard extends Component {
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyLine">{storyLine}</p>
+          <Rating className="movie-card-rating" Rating={rating} />
         </div>
       </div>
     );
@@ -23,6 +25,7 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string,
     storyLine: PropTypes.string,
     imagePath: PropTypes.string,
+    rating: PropTypes.number,
   }).isRequired,
 };
 

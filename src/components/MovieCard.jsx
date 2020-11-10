4d480c7f4d1511a43bbc1de79b435c0547383095
Jr3564 <rodolfojr94@gmail.com>
2020-11-09 22:13:02 -0300

    codeclimate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,13 +6,13 @@ export default class MovieCard extends React.Component {
     const obj = this.props.prop;
     return (
       <div className="movie-card">
-        <img src={obj.imagePath} className="movie-card-image" />
+        <img src={obj.imagePath} className="movie-card-image" alt={obj.title} />
         <div key={obj.title} className="movie-card-budy">
           <h4 className="movie-card-title">{obj.title}</h4>
           <h5 className="movie-card-subtitle">{obj.subtitle}</h5>
           <p className="movie-card-storyline">{obj.storyline}</p>
         </div>
-        <div className="movie-card-rating"><Rating rating={ obj.rating } /></div>
+        <div className="movie-card-rating"><Rating rating={obj.rating} /></div>
       </div>
     );
   }

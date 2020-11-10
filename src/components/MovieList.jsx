21b6edf7a556ac5093d9a2d179f9c8fdaca3828f
Jr3564 <rodolfojr94@gmail.com>
2020-11-09 22:17:26 -0300

    codeclimate MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MovieCard from './MovieCard.jsx';
 
-export default class MovieList extends React.Component {
+class MovieList extends React.Component {
   render() {
     return (<section className="movie-list">
-      { this.props.prop.map(movie => (<MovieCard prop={movie} />)) }
+      { this.props.prop.map((movie) => (<MovieCard prop={movie} />)) }
     </section>);
   }
 }
+
+export default MovieList;

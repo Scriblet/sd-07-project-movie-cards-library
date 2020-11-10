8468a8a6d2586185b1d38b9036a4d80f5d1c205f
Marcos R. Silva <marcos.1998226@gmail.com>
2020-11-10 13:05:27 -0300

    refluxo

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,22 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+// import Header from './Header';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div className="movie-list">{
+          movies.map((movie) => (
+            <MovieCard
+              key={movie.title} title={movie.title}
+              subtitle={movie.subtitle} storyline={movie.storyline}
+              imagePath={movie.imagePath} rating={movie.rating}
+            />
+          ))
+    } </div>
+    );
+  }
+}
+
+export default MovieList;

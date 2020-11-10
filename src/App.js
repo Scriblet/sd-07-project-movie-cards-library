747b054c80826a4cae9768ed49dee2d9c920e551
sttefanieMaciel <macielsttefanie@gmail.com>
2020-11-10 16:30:51 -0300

    Tentando novamente  commitar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header.jsx';
+import Header from '.components/Header';
 
 function App() {
   return (
     <div className="App">
-      {
-
-      }
-      
+      <h1>APP</h1>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-  //Primeiro requisito
 import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return 
+    return (
+      <h1>My first React class</h1>
+    );
   }
 }
-
 export default Header;

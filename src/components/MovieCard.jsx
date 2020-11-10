e6cec26bd1febe5494161a2c07a7faec08669a0a
Bruno Nogueira <nugobruno@gmail.com>
2020-11-10 13:47:40 -0300

    Sem erros finalmentegit add .git add .

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,12 +6,14 @@ class MovieCard extends Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
-      <div className="movie-card-body">
-        <img className="movie-card-image" src={imagePath} alt="movie thumbnail" />
-        <h4 className="movie-card-title">{title}</h4>
-        <h5 className="movie-card-subtitle">{subtitle}</h5>
-        <p className="movie-card-storyline">{storyline}</p>
-        <Rating rating={rating} />
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <img className="movie-card-image" src={imagePath} alt="movie thumbnail" />
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <Rating rating={rating} />
+        </div>
       </div>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -8,17 +8,31 @@ class MovieList extends Component {
     return (
       <div className="movie-list">
         {movies.map((movie) => (
-          <div className="movie-card">
-            <MovieCard key={movie.title} movie={movie} />
-          </div>
+          <MovieCard key={movie.title} movie={movie} />
         ))};
       </div>
     );
   }
 }
 
-MovieList.propTypes = { movies: PropTypes.oneOfType([PropTypes.object]) };
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })),
+};
 
-MovieList.defaultProps = { movies: [] };
+MovieList.defaultProps = {
+  movies: [{
+    title: 'Kingsglaive',
+    subtitle: 'Final Fantasy XV',
+    storyline: "King Regis, who oversees the land of Lucis, commands his army of soldiers to protect the kingdom from the Niflheim empire's plans to steal the sacred crystal.",
+    rating: 4.5,
+    imagePath: 'images/Kingsglaive_Final_Fantasy_XV.jpg',
+  }],
+};
 
 export default MovieList;

425c0364ce9aaf8d86feba4e0da355ce169b6930
Emanuelle Brasil <emanuelle.rbl@gmail.com>
2020-11-10 13:31:22 -0300

    100%

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
 
-// const MovieCard = (props) => {
-//   const { movie } = props;
-//   return (
-//     <div className="movie-card">
-//       {/* <img src={movie.imagePath} alt="movie" /> */}
-//     </div>
-//   );
-// };
 class MovieCard extends React.Component {
   render() {
-    return (<span>MovieCard</span>);
+    const { movie } = this.props;
+    return (
+      <div className="movie-card">
+        <img src={movie.imagePath} alt="movie" className="movie-card-image" />
+        <section className="movie-card-body">
+          <h4 className="movie-card-title">{movie.title}</h4>
+          <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
+          <p className="movie-card-storyline">{movie.storyline}</p>
+        </section>
+        <Rating rating={movie.rating} />
+      </div>
+    );
   }
 }
 
 export default MovieCard;
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
-// const MovieList = (props) => {
-//   const { movies } = props;
-//   return (
-//     <div className="movie-list">
-//       {movies.map((movie) => <MovieCard key={movie.title} data={movie} />)}
-//     </div>
-//   );
-// };
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <div className="movie-list">
-        <span>MovieList</span>
-        <MovieCard />
+        {movies.map((movie) => (<MovieCard key={movie.title} movie={movie} />))}
       </div>
     );
   }
 }
 
 export default MovieList;
+
+MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };

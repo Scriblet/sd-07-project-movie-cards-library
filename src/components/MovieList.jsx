c6b8a4547997bd3ad1918f4c60f37c3693232ffa
afonsofn <afonsoafn@gmail.com>
2020-11-10 11:21:58 -0300

    finalizando projeto CCfix2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import MovieCard from './MovieCard';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const movies = this.props.movies;
-      return(
-        <div className="movie-list">
-          {movies.map(movie => <MovieCard movie={movie} key={movie.title}/>)}
-        </div>
+    return (
+      <div className="movie-list">
+        {movies.map((movie) => <MovieCard movie={movie} key={movie.title} />)}
+      </div>
     );
   }
 }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
-    render() {
-        const rating = this.props.rating;
-        return(
-            <div className="movie-card-rating">
-                <p className="rating">{rating}</p>
-            </div>
-        );
-    }
+  render() {
+    const rating = this.props.rating;
+    return (
+      <div className="movie-card-rating">
+        <p className="rating">{rating}</p>
+      </div>
+    );
+  }
 }
 
 Rating.propTypes = { rating: PropTypes.number.isRequired };

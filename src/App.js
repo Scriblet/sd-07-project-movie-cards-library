e2b43828c887cb88790b42f87639eaf308b265d9
Lucas Ribeiro <martins7ribeiro@gmail.com>
2020-11-09 23:24:51 -0300

    CC Issues 2

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import MovieList from './components/MovieList';
 import data from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <Header />
-        {/* The 'movies' props given to the MovieList component gets its
-        information from the data imported at the beginning of the file.
-        It's used internally in the MovieList component file. */}
-        <MovieList movies={data} />
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div>
+				<Header />
+				{/* The 'movies' props given to the MovieList component gets its
+				information from the data imported at the beginning of the file.
+				It's used internally in the MovieList component file. */}
+				<MovieList movies={data} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,13 +2,13 @@
 import React from 'react';
 
 class Rating extends React.Component {
-  render() {
-    // Here I am declaring that 'rating' is a props of the Rating component.
-    const { rating } = this.props;
-    return (
-    <div className="rating">{rating}</div>
-    );
-  }
+	render() {
+		// Here I am declaring that 'rating' is a props of the Rating component.
+		const { rating } = this.props;
+		return (
+		<div className="rating">{rating}</div>
+		);
+	}
 }
 
 export default Rating;

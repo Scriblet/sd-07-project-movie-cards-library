341b1aacee46b44bda024905230d751a623cfc0b
TIAGO <tiagoesdrasvieira@gmail.com>
2020-11-09 22:06:20 -0300

    Desafio 11, 12, 13, 14, 15, 16 e 17

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath } = this.props.movie;
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
       <div>
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
         <img src={imagePath} alt={imagePath} />
+        <Rating rating={rating} />
       </div>
     );
   }
@@ -20,6 +22,7 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
     imagePath: PropTypes.string,
+    rating: PropTypes.number,
   }).isRequired,
 };
 export default MovieCard;

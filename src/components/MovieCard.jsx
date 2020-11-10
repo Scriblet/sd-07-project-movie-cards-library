cd6233c539e0d3fa85f7de246f5c8e3819361f21
Aurelio Scarante <aurelioscarante25@gmail.com>
2020-11-10 07:54:55 -0300

    :see_no_evil: Requisitos 1, 2, 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,9 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return <spam>Funciona</spam>;
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,9 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return console.log(this.props);
+  }
+}
+
+export default MovieList;

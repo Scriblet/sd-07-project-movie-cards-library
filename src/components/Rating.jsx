f9f0f36e80d437739247e2d968d6dd4c2de1b263
Lets <letsferreiratx@gmail.com>
2020-11-09 22:06:00 -0300

    cc ok

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
       <div className="rating">
-        {this.props.rating}
+        {rating}
       </div>
     );
   }
 }
+Rating.propTypes = { rating: PropTypes.shape({ rating: PropTypes.number }).isRequired };
 
 export default Rating;

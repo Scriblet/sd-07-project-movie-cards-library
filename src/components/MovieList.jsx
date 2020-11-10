ea25ba2f4beeef9f5342e577f05e2eeefb71a872
dennerpl <denner.souzapl@gmail.com>
2020-11-10 19:11:29 -0300

    movie-cards-list semi pronto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class Pokedex extends React.Component {
+  render() {
+    const movieList = this.props.movies;
+    return (
+      <div className='movie-list'>
+        {movieList.map((movie) => {
+          return <MovieCard key={movie.title} movies={movie} />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default Pokedex;

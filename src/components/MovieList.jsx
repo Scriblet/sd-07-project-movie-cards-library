9931835b640c4db0461f8d22712f30f9369dc9ac
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-11-10 14:40:26 -0300

    Ajustando css - 10/11/2020

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -10,14 +10,22 @@ class MovieList extends React.Component {
       <div className="movie-list">
         {
           movies.map((movie) =>
-            (<MovieCards movie={movie} key={movie.title} className="movie-card" />))
+            (<MovieCards movie={movie} key={movie.title} />))
         }
       </div>
     );
   }
 }
 
-MovieList.propTypes = { movies: PropTypes.arrayOf().isRequired };
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  })).isRequired,
+};
 
 MovieList.defaultProps = { movies: [] };
 

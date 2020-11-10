f98896b5b3485a70a47d692898eb825611f8b7a5
renatomak <renato.mark.silva@gmail.com>
2020-11-09 21:27:45 -0300

    requisito 5-CC-10

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -8,7 +8,7 @@ class MovieList extends Component {
     const movies = this.props.movies;
     return (
       <main>
-        <div>{ movies.map((it, i, ar) => < MovieCard key={ar.toString()} movie={it} />) }</div>
+        <div>{movies.map((it, i, ar) => < MovieCard key={ar.toString()} movie={it} />) }</div>
       </main>
     );
   }

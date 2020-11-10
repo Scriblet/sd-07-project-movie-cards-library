791b8e38ffcc137ac4ca09fcd2233d02a5218784
Galaraz <jvictor.si.2013@gmail.com>
2020-11-10 11:06:20 -0300

    projeto 100 sem css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,30 @@
-// implement MovieCard component here
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+class MovieCard extends React.Component {
+    render() {
+        const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    return (
+      <div>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <img src={imagePath} alt={imagePath} />
+        <Rating rating={rating} />
+      </div>
+    );
+  }
+}
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+
+}
+
+export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,14 @@
 // implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types'
+class Rating extends React.Component {
+    render() {
+       const rating = this.props.rating;
+       
+        return (<div className ='rating'>{rating}</div>);
+    }
+}
+
+Rating.protoTypes ={ rating: PropTypes.number.isRequired};
+
+export default Rating;

3180e015ab8062e9d26f484b92c79dbb22277339
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-11-10 00:53:34 -0300

    só falta passar os warnings

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,26 +7,28 @@ import Rating from './Rating';
 class MovieCard extends Component {
   render() {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
-    console.log(`Chegada da data no movieCard ${title}`);
+    // console.log(`Chegada da data no movieCard ${title}`);
     return (
-      <div className="movie-card">
-        <img className="movie-card-image" alt="" src={imagePath} />
-        <h4 className="movie-card-title">{title}</h4>
-        <h5 className="movie-card-subtitle">{subtitle}</h5>
-        <p className="movie-card-storyline">{storyline}</p>
-        <Rating rating={rating} />
-      </div>
+      <section className="movie-card">
+        <div className="movie-card-body">
+          <img className="movie-card-image" alt="" src={imagePath} />
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <Rating rating={rating} />
+        </div>
+      </section>
     );
   }
 }
 
 MovieCard.propTypes = {
-  movie: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
-  storyline: PropTypes.string.isRequired,
-  rating: PropTypes.string.isRequired,
-  imagePath: PropTypes.string.isRequired,
+  movie: PropTypes.arrayOf(PropTypes.object).isRequired,
+  // title: PropTypes.string.isRequired,
+  // subtitle: PropTypes.string.isRequired,
+  // storyline: PropTypes.string.isRequired,
+  // rating: PropTypes.string.isRequired,
+  // imagePath: PropTypes.string.isRequired,
 };
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -8,14 +8,14 @@ class MovieList extends Component {
   render() {
     // dentro da prop (que é um obj)vem tudo que vc passou, por isso tem que destruturar a props
     const { movies } = this.props;
-    console.log(`Chegada da data no mavelist ${movies}`);
+    // console.log(`Chegada da data no mavelist ${movies}`);
     return (<div className="movie-list">{
       movies.map((eachMovie) => <MovieCard name={eachMovie.title} movie={eachMovie} />)
     }</div>);
   }
 }
 
-MovieList.propTypes = { movies: PropTypes.string.isRequired };
+MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
 // Mas ele não é do tipo object? porque só foi com string?
 // tem que estar antes do export, provavelmente o export é a ultima coisa
 

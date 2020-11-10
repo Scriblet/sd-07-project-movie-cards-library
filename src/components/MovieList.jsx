96e923c3c3176c03679ca663d9a6b45b3ba81450
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-11-10 14:48:07 -0300

    Ajustando css final - 10/11/2020

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -18,15 +18,7 @@ class MovieList extends React.Component {
   }
 }
 
-MovieList.propTypes = {
-  movies: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    subtitle: PropTypes.string.isRequired,
-    storyline: PropTypes.string.isRequired,
-    imagePath: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired,
-  })).isRequired,
-};
+MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
 
 MovieList.defaultProps = { movies: [] };
 

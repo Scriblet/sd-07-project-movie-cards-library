4e21e7c9fcf13489e5e5b6306ad965183a9bfe14
Elano Evaristo <elano3004@gmail.com>
2020-11-10 17:53:16 -0300

    upando os arquivos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,34 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCards extends React.Component {
+  render() {
+    const movie = this.props.movie;
+    const { title, subtitle, storyline, imagePath } = this.props.movie;
+    return (
+      <div className="movie-card">
+        <img className="movie-card-image" src={imagePath} alt={`${title} sprite`} />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <Rating className="movie-card-rating" rating={movie.rating} />
+        </div>
+      </div>
+    );
+  }
+}
+
+MovieCards.defaultProps = { movie: {} };
+
+MovieCards.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    imagePath: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+  }),
+};
+
+export default MovieCards;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,26 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCards from './MovieCard';
+
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
+        {movies.map((movie) => <MovieCards key={movie.title} movie={movie} />)}
+      </div>
+    );
+  }
+}
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  })),
+};
+MovieList.defaultProps = { movies: [] };
+export default MovieList;

0af90a871c8b949c60be5a20cac5971888e413f5
Arthur Nunes <coletivosuburbano@gmail.com>
2020-11-09 23:12:55 -0300

    corrigindo CodeClimate

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import MovieCard from './MovieCard';
-import PropTypes from 'prop-types';
+
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
       <div className="movie-list">
-        {movies.map(movie => <MovieCard movie={movie} className="movie-card" />)}
+        {movies.map((movie) => <MovieCard movie={movie} className="movie-card" />)}
       </div>
     );
   }
 }
-MovieList.propTypes = {
-  movies: PropTypes.array
-}
-
 
 export default MovieList;

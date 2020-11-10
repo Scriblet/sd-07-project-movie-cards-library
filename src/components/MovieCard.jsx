3172c13985346e398ca18f12e64f9769615741e6
brendalima <brenda.c.lima@poli.ufrj.br>
2020-11-10 18:30:42 -0300

    polishing CSS features

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,11 +9,13 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
       <div className="movie-card">
-        <img className="movie-card-image" alt="" src={imagePath} />
-        <h4 className="movie-card-title">{title}</h4>
-        <h5 className="movie-card-subtitle">{subtitle}</h5>
-        <p className="movie-card-storyline">{storyline}</p>
-        <Rating rating={rating} />
+        <div className="movie-card-body">
+          <img className="movie-card-image" alt="" src={imagePath} />
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
+          <Rating rating={rating} />
       </div>
     );
   }

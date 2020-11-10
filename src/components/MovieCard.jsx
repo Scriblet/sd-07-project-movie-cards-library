f3027a8f302adacb3a22d0b139457a018a1e5b75
Lucas Miranda <lucasmiranda@pop-os.localdomain>
2020-11-10 12:24:55 -0300

    atualizaçao dos arquivos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,20 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+      <div>
+        <img ClassName="card-image" alt={title} src={imagePath} />
+        <div className="info">
+          <h6 className="title">{title}</h6>
+          <p className="subtitle">{subtitle}</p>
+          <p className="storyline">{storyline}</p>
+          <p className="rating">{rating}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <div>
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MovieList;

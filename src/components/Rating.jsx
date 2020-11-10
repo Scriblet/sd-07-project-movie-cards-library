6db5a4ba3d777c148a4fc1837afe45edaeb67e04
Strongreen <strongreen@live.com>
2020-11-10 10:06:54 -0300

    Correçoes Props

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
-
 import PropTypes from 'prop-types';
-
-
-Rating.propTypes = { rating: PropTypes.number.isRequired };
-
-function Ratings(props) {
-  rating = props.rating;
-  return (
-    <div key={rating.toString()} className="rating">
-      <strong>{ rating }</strong>
-    </div>
-  );
-}
 
 class Rating extends React.Component {
   render() {
-    return Ratings(this.props);
+    const rating = this.props.rating;
+    return (
+      <div key={rating.toString()} className="rating">
+        {rating}
+      </div>
+    );
   }
 }
+
+Rating.propTypes = { rating: PropTypes.number.isRequired };
 
 export default Rating;

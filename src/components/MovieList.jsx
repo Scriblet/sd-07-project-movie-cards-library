22796927541dc46e64edb04798cda109e22b254f
Filipi Firmino <contatoic3web@gmail.com>
2020-11-09 22:58:31 -0300

    Refactoring CC

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -14,6 +14,6 @@ class MovieList extends React.Component {
   }
 }
 
-MovieList.propTypes = {movies: propTypes.arrayOf(propTypes.object).isRequired};
+MovieList.propTypes = { movies: propTypes.arrayOf(propTypes.object).isRequired };
 
 export default MovieList;

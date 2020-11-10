00b5e247691b6a9e8f526e110069fb2586b2b4e3
Rogerio-feitor <roger.feitor@gmail.com>
2020-11-09 21:30:48 -0300

    Consertando os erros codeclimate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+
 class Header extends React.Component {
   render() {
     return (
-      <header className = 'page-title'>
-          <h1>Movie Cards Library</h1>
+      <header className="movie-card-header">
+        <h1 className="page-title">Movie Cards Library</h1>
       </header>
-    )
+    );
   }
 }
 export default Header;

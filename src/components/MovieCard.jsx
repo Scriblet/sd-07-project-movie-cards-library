87a0f889aa7345fe99304c4fba7d85162935b72b
Jr3564 <rodolfojr94@gmail.com>
2020-11-10 00:39:51 -0300

    MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,16 +4,15 @@ import Rating from './Rating.jsx';
 export default class MovieCard extends React.Component {
   render() {
     const properties = this.props;
-    const { imagePath, title, subtitle, storyline, rating, key } = properties.prop;
     return (
-      <div className="movie-card" key={key}>
-        <img src={imagePath} className="movie-card-image" alt={title} />
+      <div className="movie-card" key={properties.prop.key}>
+        <img src={properties.prop.imagePath} className="movie-card-image" alt={properties.prop.title} />
         <div className="movie-card-budy">
-          <h4 className="movie-card-title">{title}</h4>
-          <h5 className="movie-card-subtitle">{subtitle}</h5>
-          <p className="movie-card-storyline">{storyline}</p>
+          <h4 className="movie-card-title">{properties.prop.title}</h4>
+          <h5 className="movie-card-subtitle">{properties.prop.subtitle}</h5>
+          <p className="movie-card-storyline">{properties.prop.storyline}</p>
         </div>
-        <div className="movie-card-rating"><Rating rating={rating} /></div>
+        <div className="movie-card-rating"><Rating rating={properties.prop.rating} /></div>
       </div>
     );
   }

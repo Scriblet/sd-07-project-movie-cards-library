b58141908237eb2a9eefe56371fa2e914af24b28
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2020-11-09 21:05:34 -0300

    Projeto finalizado e de acordo com CC

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import '../App.css';
 
 class Header extends React.Component {
 
-    render() {
-        
-    }
+  render() {
+    return (<header className="movie-card-header">
+      <h1 className="page-title">Movie Cards Library</h1>
+    </header>);
+  }
 }
 
 export default Header;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,35 @@
-// implement MovieCard component here
+import PropTypes from 'prop-types';
+import React from 'react';
+import Rating from './Rating';
+import '../App.css';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    return (
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <img className="movie-card-image" src={imagePath} alt="Imagem do filme" />
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <Rating className="movie-card-rating" rating={rating} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.number,
+  }),
+};
+
+MovieCard.defaultProps = { movie: {} };

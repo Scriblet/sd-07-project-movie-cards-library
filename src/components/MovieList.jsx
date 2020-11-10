0d7c9b5a4ecae67741f6a0b1443bf6168aef2eb4
LAURO DE SA <itrafor@gmail.com>
2020-11-09 21:08:22 -0300

    Projeto concluido

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,24 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+export default function MovieList(props) {
+  const list = props.movies;
+  return (
+    <div>
+      {list.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+    </div>
+  );
+}
+
+
+MovieList.propTypes = {
+  movies: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }),
+};
+
+MovieList.defaultProps = { movies: { notes: 'No movies Information' } };

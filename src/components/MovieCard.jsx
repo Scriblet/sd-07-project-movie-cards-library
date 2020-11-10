7be8dc0755e700c907cfde630a5bc48f330ed85f
Ana Karine <anakarine.ti@gmail.com>
2020-11-10 01:15:05 -0300

    <MovieList /> component and <App /> component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title } = this.props.movie;
+    return (
+      <p>{title}</p>
+    );
+  }
+}
+
+MovieCard.propTypes = { movie: PropTypes.shape({ title: PropTypes.string }).isRequired };
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div className="movie-list">
+        {this.props.movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+      </div>
+    );
+  }
+}
+
+// https://medium.com/@fannyvieira/proptypes-in-react-eb5f9d417fb
+MovieList.propTypes = { movies: PropTypes.arrayOf({ title: PropTypes.string }).isRequired };
+
+export default MovieList;

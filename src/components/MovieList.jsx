2fd504a9e79fddd07bd04bb09e55df6db02bb6f2
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-11-10 09:08:20 -0300

    Ajusting code climate

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,7 @@
 // implement MovieList component here
 
 import React from 'react';
-import data from '../data';
+// import data from '../data';
 // import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {

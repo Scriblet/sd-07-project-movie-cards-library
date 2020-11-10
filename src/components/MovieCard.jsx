8d4abfe7ab39df13e94bfcb102ccf39bc1b599ce
Elano Evaristo <elano3004@gmail.com>
2020-11-10 19:06:41 -0300

    correcao no rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,8 +4,7 @@ import Rating from './Rating';
 
 class MovieCards extends React.Component {
   render() {
-    const movie = this.props.movie;
-    const { title, subtitle, storyline, imagePath } = this.props.movie;
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <div className="movie-card">
         <img className="movie-card-image" src={imagePath} alt={`${title} sprite`} />
@@ -13,22 +12,21 @@ class MovieCards extends React.Component {
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
-          <Rating className="movie-card-rating" rating={movie.rating} />
         </div>
+            <Rating rating={rating} />
       </div>
     );
   }
 }
 
-MovieCards.defaultProps = { movie: {} };
+export default MovieCards;
 
 MovieCards.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string,
-    imagePath: PropTypes.string,
-    subtitle: PropTypes.string,
-    storyline: PropTypes.string,
-  }),
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
 };
-
-export default MovieCards;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,15 +3,14 @@ import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    const { rating } = this.props;
     return (
       <div className="movie-card-rating">
-        <p className="rating">{rating}</p>
+        Rating:<span className="rating">{this.props.rating}</span>
       </div>
     );
   }
 }
 
-Rating.propTypes = { rating: PropTypes.number.isRequired };
-
 export default Rating;
+
+Rating.propTypes = { rating: PropTypes.number.isRequired };

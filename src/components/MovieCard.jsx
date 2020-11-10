bf816b6aa2841ba777734e8fb6e2bac38c7d631b
Matheus Beaumord <matheusbeaumord@gmail.com>
2020-11-10 14:29:56 -0300

    Envio do projeto com todos os requisitos aprovados pelo Jest Test e Codeclimate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,35 @@
-// implement MovieCard component here
+import PropTypes from 'prop-types';
+import React from 'react';
+import Rating from './Rating';
+import '../App.css';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    return (
+      <div className="movie-card">
+        <img className="movie-card-image" src={imagePath} alt={title} />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
+        <div className="movie-cart-ratting">
+          <Rating rating={rating} />
+        </div>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    imagePath: PropTypes.string,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,22 @@
-// implement MovieList component here
+import PropTypes from 'prop-types';
+import React from 'react';
+import MovieCard from './MovieCard';
+import '../App.css';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
+        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+      </div>
+    );
+  }
+}
+
+const aux = PropTypes.shape({ title: PropTypes.string });
+MovieList.propTypes = { movies: PropTypes.arrayOf(aux).isRequired };
+
+// MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
+
+export default MovieList;

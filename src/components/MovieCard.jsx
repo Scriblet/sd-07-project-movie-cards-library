1b6255b7e56bae3d8ecdee82569b89a632dca2d9
Jr3564 <rodolfojr94@gmail.com>
2020-11-10 01:51:31 -0300

    MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,27 +3,27 @@ import React from 'react';
 import Rating from './Rating.jsx';
 
 export default class MovieCard extends React.Component {
-  render() {
+  render(porps) {
     const properties = this.props;
-    const obj = properties.prop;
+    const proper = properties.prop;
     return (
       <div className="movie-card">
-        <img src={obj.imagePath} className="movie-card-image" alt={obj.title} />
+        <img src={proper.imagePath} className="movie-card-image" alt={proper.title} />
         <div className="movie-card-budy">
-          <h4 className="movie-card-title">{obj.title}</h4>
-          <h5 className="movie-card-subtitle">{obj.subtitle}</h5>
-          <p className="movie-card-storyline">{obj.storyline}</p>
+          <h4 className="movie-card-title">{proper.title}</h4>
+          <h5 className="movie-card-subtitle">{proper.subtitle}</h5>
+          <p className="movie-card-storyline">{proper.storyline}</p>
         </div>
-        <div className="movie-card-rating"><Rating rating={obj.rating} /></div>
+        <div className="movie-card-rating"><Rating rating={proper.rating} /></div>
       </div>
     );
   }
 }
 
 MovieCard.propTypes = {
-  "prop.imagePath": PropTypes.string,
-  "prop.title": PropTypes.string,
-  "prop.subtitle": PropTypes.string,
-  "prop.storyline": PropTypes.string,
-  "prop.rating":  PropTypes.number,
+  imagePath: PropTypes.string,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
+  rating:  PropTypes.number,
 };

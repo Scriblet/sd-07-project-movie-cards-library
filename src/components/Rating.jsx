8dadd642273c7f1ce7a546a981afca81c06f96cc
renatomak <renato.mark.silva@gmail.com>
2020-11-09 21:58:48 -0300

    requisito 11 e 12-2

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
+      const rating = this.props.rating;
     return (
         <span>
-            {this.props.rating}
+            {rating}
         </span>
     );
   }
 }
+
+Rating.propTypes = { rating: PropTypes.string };
 
 export default Rating;

2caa0b4ac2172a6ef1183cd339cec510f8b9d814
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-11-10 04:23:16 -0300

    Ajustando cc - 10/11/2020

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -21,14 +21,17 @@ class MovieCard extends React.Component {
 }
 
 MovieCard.propTypes = {
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
-  storyline: PropTypes.string.isRequired,
-  imagePath: PropTypes.string.isRequired,
-  rating: PropTypes.string.isRequired,
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.string.isRequired,
+  })
 };
 
 MovieCard.defaultProps = {
+  movie: {},
   title: 'Título',
   subtitle: 'Subtítulo',
   storyline: 'Resumo',

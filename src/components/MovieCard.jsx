5ebf91f05ce5135d46d278b047efd6e71698de1a
Jr3564 <rodolfojr94@gmail.com>
2020-11-09 23:44:33 -0300

    Mecanismo funcionando

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,16 +4,16 @@ import Rating from './Rating.jsx';
 export default class MovieCard extends React.Component {
   render() {
     const properties = this.props;
-    const { imagePath, title, subtitle, storyline, rating } = properties.children;
+    const { imagePath, title, subtitle, storyline, ratingObj } = properties.children;
     return (
-      <div className="movie-card">
+      <div key={title} className="movie-card">
         <img src={imagePath} className="movie-card-image" alt={title} />
-        <div key={title} className="movie-card-budy">
+        <div className="movie-card-budy">
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
         </div>
-        <div className="movie-card-rating"><Rating rating={rating} /></div>
+        <div className="movie-card-rating"><Rating rating={ratingObj} /></div>
       </div>
     );
   }

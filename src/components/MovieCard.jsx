6b9c7a3a987aafd233879ad32b2685a94e282e5d
Renato Silva <renatosdev@gmail.com>
2020-11-10 22:33:37 -0300

    Faltando apenas os requisitos 12, 13 e 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 class MovieCard extends React.Component {
   render() {
-    // const { title, subtitle, storyline, imagePath, rating } = this.props.movies;
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
-      <div>teste</div>
+      <div>
+        <img src={imagePath} alt="" />;
+        <h4>{title}</h4>;
+        <h5>{subtitle}</h5>;
+        <p>{storyline}</p>;
+     </div>
     );
   }
+}
+
+MovieCard.protoType = {
+movies: PropTypes.shape({
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string
+})
 }
 
 export default MovieCard;

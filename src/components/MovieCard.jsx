600d4785dae1b815006ff1c34c18ef31aa3a16c7
moacyrrln <moacyrrln@gmail.com>
2020-11-11 19:32:35 -0300

    codeclimate22

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -24,7 +24,10 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string,
     subtitle: PropTypes.string,
-  })
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired
 };
 
 export default MovieCard;

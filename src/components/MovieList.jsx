a3508331b10a6263a17c36592aa547c21cf9dfd0
sttefanieMaciel <macielsttefanie@gmail.com>
2020-11-11 15:46:26 -0300

    Quarto  Requisito

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
 
 class MovieList extends React.Component {
   render() {
+    const arrayMovies = this.props.movies;
     return (
       <div className="movie-list">
-        <h1>Movie</h1>
+        { arrayMovies.map((element) => <MovieCard key={element.title} movie={element} />) }
       </div>
     );
   }
 }
+MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
 export default MovieList;

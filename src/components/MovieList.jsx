9d4c739913d04cbbbc1f342201a81ea941fcc23e
Camila Valentim <camilaifpb@hotmail.com>
2020-11-11 20:08:35 -0300

    movie list

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (<div>
+        {this.props.movies[0]};
+    </div>
+    );
+  }
+}
+
+export default MovieList;

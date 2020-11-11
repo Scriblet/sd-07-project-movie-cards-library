1280277a679653b00c2d1da18844043a6f54a9c9
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-11 20:25:24 -0300

    Commit: 0.0.3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,10 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    const movies = this.props;
+    return <div>{movies}</div>;
+  }
+}
+
+export default MovieList;

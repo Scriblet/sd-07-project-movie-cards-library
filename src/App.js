6295629d4a6de4c0f75c9a1ee76da28b94343927
Breenolf <blf@ic.ufal.br>
2020-11-11 10:22:09 -0300

    Corrigindo props do Componente MovieList

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Movielist from './components/MovieList';
-import MoviesObject from './data';
+import Data from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Movielist movie={MoviesObject} />
+      <Movielist movies={Data} />
     </div>
   );
 }

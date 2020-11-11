67ef3b4f6568ddb3de949eb0514f8becdea77451
Brune Miguel <bstfreire@gmail.com>
2020-11-11 12:34:20 -0300

    Corrigindo CC

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
-//Passe uma key para cada MovieCard renderizado
 class MovieList extends React.Component {
   render() {
     const movies = this.props.movies;
     return (
       <div>
-        {movies.map((movie) => <MovieCard  key={movie.title} movie={movie} />)}
+        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
   }

e6f51d8bcaca8092cc24e56034b98338bc76102a
Thays Costa <thayscosta3@gmail.com>
2020-11-11 13:05:05 -0300

    All requirements with Codeclimate adjustments

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,25 @@
-// implement MovieCard component here
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import Rating from './Rating';
+
+
+class MovieCard extends Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <div>
+        <img src={movie.imagePath} alt={movie.title} />
+        <div>
+          <h4>{movie.title}</h4>
+          <h5>{movie.subtitle}</h5>
+          <p>{movie.storyline}</p>
+          <Rating rating={movie.rating} />
+        </div>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = { movie: PropTypes.string.isRequired };
+
+export default MovieCard;

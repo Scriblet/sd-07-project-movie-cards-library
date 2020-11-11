fd5c8b31398599f6d770e6fe5aae9fe814532672
Renato Silva <renatosdev@gmail.com>
2020-11-10 21:56:55 -0300

    Requisito 4,5 e 6 - codiclimate

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,12 +3,11 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props;
     return (
       <div>
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {this.props.movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
-     );
+    );
   }
 }
 

0cf725c6d2a225e44070c48f56dc4a2666fb190b
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-11-10 23:47:23 -0300

    Alteraçoes para o decimo primeiro ao decimo quarto requisitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const properties = this.props;
-    const { title, subtitle, imagePath, storyline } = properties.movie;
+    const { title, subtitle, imagePath, storyline, rating } = properties.movie;
     return (
       <div>
         <img src={imagePath} alt="" />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
+        <Rating rating={rating} />
       </div>
     );
   }

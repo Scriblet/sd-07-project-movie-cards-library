ea261f8c0aec9afa6b2f5e05b00a094ed3417133
sttefanieMaciel <macielsttefanie@gmail.com>
2020-11-11 16:28:09 -0300

    Corrigindo cc no quarto requisito

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     return (
-      <header>
-        <h1>MovieCard</h1>
-      </header>
+      <p>MovieCard</p>
     );
   }
 }
